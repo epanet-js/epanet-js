43f54699a21d65cabc9c84432b6c1d09f69b7311
Sam Gathercole <sapagat@fastmail.com>
2024-10-16 11:36:19 +0200

    Add auth basic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,31 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+const basicAuthUser = process.env.BASIC_AUTH_USER || 'matrado'
+const basicAuthPassword = process.env.BASIC_AUTH_PASSWORD || 'matrado'
+
 export function middleware(request: NextRequest) {
+  const authHeader = request.headers.get('Authorization');
+  if (!authHeader) {
+    return new NextResponse('Authentication required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    });
+  }
+  const base64Credentials = authHeader.split(' ')[1];
+  const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+  const [username, password] = credentials.split(':');
+
+  if (basicAuthUser !== username || basicAuthPassword !== password) {
+    return new NextResponse('Invalid credentials', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
   return NextResponse.next()
 }
 
