791d7e5a75e29b1b4bca9491f6a61bb44190018e
Sam Gathercole <sapagat@fastmail.com>
2024-10-18 14:15:34 +0200

    Translate pipe hints

--- src/infra/i18n/locales/en.ts
+++ src/infra/i18n/locales/en.ts
@@ -1,6 +1,8 @@
 export const translations = {
   select: "Select",
   junction: "Junction",
-  pipe: "Pipe"
+  pipe: "Pipe",
+  hintPipeDrawStart: "Click to start the pipe, then click to add each vertex.",
+  hintPipeDrawEnd: "End a pipe by double-clicking or hitting Enter."
 }
 
--- src/infra/i18n/locales/es.ts
+++ src/infra/i18n/locales/es.ts
@@ -3,6 +3,8 @@ import { Translations } from './locale'
 export const translations: Translations  = {
   select: "Seleccionar",
   junction: "Nudo",
-  pipe: "Tubería"
+  pipe: "Tubería",
+  hintPipeDrawStart: "Haz click para empezar un tubería, después haz click para añadir vértices.",
+  hintPipeDrawEnd: "Finaliza una tubería con doble click o apretando Enter."
 }
 
