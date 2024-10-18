b6d881bb4aff55f8c216645fae10450cec425e28
Sam Gathercole <sapagat@fastmail.com>
2024-10-18 13:30:02 +0200

    Log to console errors in debug mode

--- src/infra/error-tracking.ts
+++ src/infra/error-tracking.ts
@@ -1,15 +1,24 @@
 import * as Sentry from '@sentry/nextjs'
 
+const isDebugMode = (): boolean => process.env.NODE_ENV === "development"
+
 export const captureError = (error: Error) => {
+  // eslint-disable-next-line no-console
+  if (isDebugMode()) console.error(error)
+
   Sentry.captureException(error)
 }
 
 export const captureWarning = (message: string) => {
+  // eslint-disable-next-line no-console
+  if (isDebugMode()) console.warn(message)
+
   Sentry.captureMessage(message, 'warning')
 }
 
 export const addToErrorLog = (breadcrumbs: Sentry.Breadcrumb) => {
     Sentry.addBreadcrumb(breadcrumbs)
 }
+
 
 export const ErrorBoundary = Sentry.ErrorBoundary
