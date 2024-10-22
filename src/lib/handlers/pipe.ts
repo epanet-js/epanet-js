d7b3994edfa98eb1bb09a233c733a6f36d6acf6c
Sam Gathercole <sapagat@fastmail.com>
2024-10-22 14:21:39 +0200

    Support undo/redo when drawing pipes

--- src/lib/handlers/pipe.ts
+++ src/lib/handlers/pipe.ts
@@ -1,21 +1,27 @@
 import { USelection } from "src/state";
-import type { HandlerContext, IFeature, LineString, Position } from "src/types";
+import type {
+  Feature,
+  HandlerContext,
+  IFeature,
+  IWrappedFeature,
+  LineString,
+  Position,
+} from "src/types";
 import {
   selectionAtom,
   modeAtom,
   Mode,
   cursorStyleAtom,
+  ephemeralStateAtom,
 } from "src/state/jotai";
-import * as utils from "src/lib/map_component_utils";
 import replaceCoordinates from "src/lib/replace_coordinates";
 import { useSetAtom } from "jotai";
 import { usePopMoment } from "src/lib/persistence/shared";
 import { CURSOR_DEFAULT } from "src/lib/constants";
-import { createOrUpdateFeature, getMapCoord } from "./utils";
+import { getMapCoord } from "./utils";
 import { useRef } from "react";
-import { lockDirection, useShiftHeld } from "src/hooks/use_held";
-import { captureError, captureWarning } from "src/infra/error-tracking";
-import { trackUserAction } from "src/infra/user-tracking";
+import { captureError } from "src/infra/error-tracking";
+import { newFeatureId } from "../id";
 
 export function usePipeHandlers({
   rep,
@@ -27,121 +33,152 @@ export function usePipeHandlers({
 }: HandlerContext): Handlers {
   const multi = mode.modeOptions?.multi;
   const setSelection = useSetAtom(selectionAtom);
+  const setEphemeralState = useSetAtom(ephemeralStateAtom);
   const setMode = useSetAtom(modeAtom);
   const setCursor = useSetAtom(cursorStyleAtom);
   const transact = rep.useTransact();
   const popMoment = usePopMoment();
   const usingTouchEvents = useRef<boolean>(false);
-  const shiftHeld = useShiftHeld();
+  const drawingStart = useRef<Pos2 | null>(null);
+
+  const setDrawingState = (features: IWrappedFeature[]) => {
+    setEphemeralState({
+      type: "drag",
+      features,
+    });
+  };
+
+  const resetDrawingState = () => {
+    setEphemeralState({ type: "none" });
+  };
+
+  const createExtensionFeature = (start: Pos2, end: Pos2) => {
+    return {
+      id: newFeatureId(),
+      feature: {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "LineString",
+          coordinates: [start, end],
+        },
+      } as Feature,
+      folderId: null,
+      at: "any",
+    };
+  };
+
+  const extendLineString = (
+    wrappedFeature: IWrappedFeature,
+    position: Pos2,
+  ) => {
+    const feature = wrappedFeature.feature as IFeature<LineString>;
+    return {
+      ...wrappedFeature,
+      feature: replaceCoordinates(
+        feature,
+        mode.modeOptions?.reverse
+          ? [position as Position].concat(feature.geometry.coordinates)
+          : feature.geometry.coordinates.concat([position]),
+      ),
+    };
+  };
+
+  const selectFeature = (feature: IWrappedFeature) => {
+    setSelection(USelection.single(feature.id));
+  };
 
   const handlers: Handlers = {
     click: (e) => {
-      const { modeOptions } = mode;
+      const clickPosition = getMapCoord(e);
 
-      if (selection.type === "none" || selection.type === "folder") {
-        /**
-         * Drawing a new line: create the line and set the new
-         * selection
-         */
-        const lineString = utils.newLineStringFromClickEvent(e);
+      const isStarting =
+        selection.type === "none" || selection.type === "folder";
+      const isAddingVertex = selection.type === "single";
 
-        const putFeature = createOrUpdateFeature({
-          mode,
-          selection,
-          featureMap,
-          geometry: lineString,
-        });
+      if (isStarting) {
+        const extensionFeature = createExtensionFeature(
+          clickPosition,
+          clickPosition,
+        );
 
-        const id = putFeature.id;
-        trackUserAction("PIPE_ADDED");
-        transact({
-          note: "Drew a line",
-          putFeatures: [putFeature],
-        }).catch((e) => captureError(e));
-        setSelection(USelection.single(id));
-      } else if (selection.type === "single") {
-        /**
-         * Appending to an existing line. Push a coordinate
-         * onto it.
-         */
-        const position = getMapCoord(e);
-        const wrappedFeature = featureMap.get(selection.id);
+        drawingStart.current = clickPosition;
+        selectFeature(extensionFeature);
+        setDrawingState([extensionFeature]);
+        return;
+      }
+
+      if (isAddingVertex && !!drawingStart.current) {
+        const clickPosition = getMapCoord(e);
+        let wrappedFeature = featureMap.get(selection.id);
+
         if (!wrappedFeature) {
-          setSelection(USelection.none());
-          return;
+          wrappedFeature = createExtensionFeature(
+            drawingStart.current,
+            clickPosition,
+          );
+          selectFeature(wrappedFeature);
         }
-        const feature = wrappedFeature.feature as IFeature<LineString>;
-        void transact({
-          note: "Added to line",
-          putFeatures: [
-            {
-              ...wrappedFeature,
-              feature: replaceCoordinates(
-                feature,
-                modeOptions?.reverse
-                  ? [position as Position].concat(feature.geometry.coordinates)
-                  : feature.geometry.coordinates.concat([position]),
-              ),
-            },
-          ],
-        })
-          .catch((e) => captureError(e))
-          .then(() => {
-            // console.log("processed click");
-          });
+
+        const updatedLineString = extendLineString(
+          wrappedFeature,
+          clickPosition,
+        );
+
+        transact({
+          note: "Added pipe vertex",
+          putFeatures: [updatedLineString],
+        }).catch((e) => captureError(e));
+
+        drawingStart.current = clickPosition;
       }
     },
-
-    /**
-     * These are ephemeral states. While you're moving the mouse
-     * while drawing a linestring, the final point of the line string
-     * isn't "real" yet. It's ephemeral. This sets the _state_ for that
-     * part of the line, which is combined into the data sent to the
-     * map at the very last minute.
-     */
     move: (e) => {
-      const { modeOptions } = mode;
       if (selection.type !== "single") return;
 
-      /**
-       * Ignore mousemove events produced by the Apple Pencil.
-       */
-      if (e.type === "mousemove" && usingTouchEvents.current) {
+      const isApplePencil = e.type === "mousemove" && usingTouchEvents.current;
+      if (isApplePencil) {
         return;
       }
-      const wrappedFeature = featureMap.get(selection.id);
-      if (!wrappedFeature) {
-        captureWarning("Unexpected missing wrapped feature");
-        return;
-      }
-      const feature = wrappedFeature.feature as IFeature<LineString>;
 
-      let nextCoord = getMapCoord(e) as Position;
-      const lastCoord = feature.geometry.coordinates.at(-2);
-      if (shiftHeld.current && lastCoord) {
-        nextCoord = lockDirection(lastCoord, nextCoord);
-      }
+      if (!drawingStart.current) return;
 
-      void transact({
-        putFeatures: [
-          {
-            ...wrappedFeature,
-            feature: replaceCoordinates(
-              feature,
-              modeOptions?.reverse
-                ? [nextCoord].concat(feature.geometry.coordinates.slice(1))
-                : feature.geometry.coordinates.slice(0, -1).concat([nextCoord]),
-            ),
-          },
-        ],
-        quiet: true,
-      })
-        .catch((e) => captureError(e))
-        .then(() => {
-          // console.log("processed move");
-        });
+      const extensionFeature = createExtensionFeature(
+        drawingStart.current,
+        getMapCoord(e),
+      );
+
+      setDrawingState([extensionFeature]);
     },
+    double: (e) => {
+      if (selection?.type !== "single") return;
 
+      e.preventDefault();
+
+      if (!multi) {
+        setMode({ mode: Mode.NONE });
+      } else {
+        setSelection(
+          USelection.selectionToFolder({
+            selection,
+            folderMap,
+            featureMap,
+          }),
+        );
+      }
+
+      resetDrawingState();
+
+      const extraForDoubleClick = 1;
+      popMoment(extraForDoubleClick);
+    },
+    enter() {
+      setMode({ mode: Mode.NONE });
+
+      if (selection.type !== "single") return;
+
+      resetDrawingState();
+    },
     touchstart: (e) => {
       usingTouchEvents.current = true;
       e.preventDefault();
@@ -163,79 +200,6 @@ export function usePipeHandlers({
     up() {
       dragTargetRef.current = null;
       setCursor(CURSOR_DEFAULT);
-    },
-    double: (e) => {
-      const { modeOptions } = mode;
-      if (selection?.type !== "single") return;
-
-      // Assuming that browser are following standard event order
-      // of two clicks & a dblclick, the second point should be dropped.
-      const wrappedFeature = featureMap.get(selection.id);
-      if (!wrappedFeature) {
-        setSelection(USelection.none());
-        return;
-      }
-      if (!multi) {
-        setMode({ mode: Mode.NONE });
-      } else {
-        setSelection(
-          USelection.selectionToFolder({
-            selection,
-            folderMap,
-            featureMap,
-          }),
-        );
-      }
-      e.preventDefault();
-
-      const feature = wrappedFeature.feature as IFeature<LineString>;
-      const finalFeature = replaceCoordinates(
-        feature,
-        modeOptions?.reverse
-          ? feature.geometry.coordinates.slice(2)
-          : feature.geometry.coordinates.slice(0, -2),
-      );
-      void popMoment(2);
-      transact({
-        putFeatures: [
-          {
-            ...wrappedFeature,
-            feature: finalFeature,
-          },
-        ],
-        quiet: true,
-      }).catch((e) => captureError(e));
-    },
-    enter() {
-      setMode({ mode: Mode.NONE });
-      if (selection.type !== "single") return;
-
-      // From here on out, we're re-entering this line mode. There's
-      // already a line on the map, and we are continuing it.
-      const selected = featureMap.get(selection.id);
-
-      if (!selected) {
-        setSelection(USelection.none());
-        return;
-      }
-
-      const feature = selected.feature as IFeature<LineString>;
-
-      transact({
-        putFeatures: [
-          {
-            ...selected,
-            feature: replaceCoordinates(
-              feature,
-              feature.geometry.coordinates.length > 2
-                ? mode.modeOptions?.reverse
-                  ? feature.geometry.coordinates.slice(1)
-                  : feature.geometry.coordinates.slice(0, -1)
-                : feature.geometry.coordinates,
-            ),
-          },
-        ],
-      }).catch((e) => captureError(e));
     },
   };
 
