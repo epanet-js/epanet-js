df3b4860b7bfbcb069ebc39051fdb4d8329f2a35
Sam Gathercole <sapagat@fastmail.com>
2024-10-30 08:36:12 +0100

    Hide halo when on link start

--- src/hydraulics/assets.ts
+++ src/hydraulics/assets.ts
@@ -1,3 +1,4 @@
+import { isSamePosition } from "src/lib/geometry";
 import { newFeatureId } from "src/lib/id";
 import replaceCoordinates from "src/lib/replace_coordinates";
 import {
@@ -74,7 +75,21 @@ export const addVertexToLink = (link: LinkAsset, position: Position) => {
   };
 };
 
-export const getNodeCoordinates = (node: NodeAsset) => {
+export const isLinkStart = (link: LinkAsset, position: Position) => {
+  const coordinates = getLinkCoordinates(link);
+  return isSamePosition(coordinates[0], position) && coordinates.length == 2;
+};
+
+export const getLinkCoordinates = (link: LinkAsset): Position[] => {
+  const { feature } = link;
+  if (!feature || !feature.geometry || feature.geometry.type !== "LineString") {
+    throw new Error("Feature is not a valid point");
+  }
+
+  return feature.geometry.coordinates;
+};
+
+export const getNodeCoordinates = (node: NodeAsset): Position => {
   const { feature } = node;
   if (!feature || !feature.geometry || feature.geometry.type !== "Point") {
     throw new Error("Feature is not a valid point");
