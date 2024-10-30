4a16d1904e5571128cce64793ec5775a718c1ae8
Sam Gathercole <sapagat@fastmail.com>
2024-10-30 09:17:42 +0100

    Place logic to build layers next to ephemeral state

--- src/lib/handlers/draw-pipe/ephemeral-state.ts
+++ src/lib/handlers/draw-pipe/ephemeral-state.ts
@@ -1,3 +1,5 @@
+import { PathStyleExtension } from "@deck.gl/extensions";
+import { GeoJsonLayer, ScatterplotLayer } from "@deck.gl/layers";
 import { Position } from "geojson";
 import { Pipe } from "src/hydraulics/assets";
 
@@ -6,3 +8,30 @@ export interface EphemeralDrawPipe {
   pipe: Pipe;
   snappingCandidate: Position | null;
 }
+
+export const buildLayers = (state: EphemeralDrawPipe) => {
+  return [
+    new GeoJsonLayer({
+      id: "DRAW_LINE",
+      data: [state.pipe.feature],
+      lineWidthUnits: "pixels",
+      getLineWidth: 4,
+      lineCapRounded: true,
+      getDashArray: [4, 4],
+      extensions: [new PathStyleExtension({ dash: true })],
+    }),
+    state.snappingCandidate &&
+      new ScatterplotLayer({
+        id: "SNAPPING_CANDIDATE",
+        data: [state.snappingCandidate],
+        getPosition: (d) => d,
+        getRadius: 10,
+        radiusUnits: "pixels",
+        stroked: true,
+        getFillColor: [255, 140, 0, 100],
+        getLineColor: [0, 0, 0],
+        getLineWidth: 1,
+        lineWidthUnits: "pixels",
+      }),
+  ];
+};
