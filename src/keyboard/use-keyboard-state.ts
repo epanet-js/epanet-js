acffc39af5878d09ee446e59239781fac952f97e
Sam Gathercole <sapagat@fastmail.com>
2024-11-04 14:29:04 +0100

    Use library functions to check if held

--- src/keyboard/use-keyboard-state.ts
+++ src/keyboard/use-keyboard-state.ts
@@ -1,41 +1,10 @@
-import { useRef } from "react";
-import { useHotkeysDeprecated } from "./hotkeys";
+import { isHotkeyPressed } from "react-hotkeys-hook";
 
 export function useKeyboardState() {
-  const shiftHeld = useRef<boolean>(false);
-  const spaceHeld = useRef<boolean>(false);
-  const controlHeld = useRef<boolean>(false);
-
-  useHotkeysDeprecated(
-    "shift",
-    (e) => {
-      shiftHeld.current = e.shiftKey;
-    },
-    { keydown: true, keyup: true },
-    [],
-  );
-
-  useHotkeysDeprecated(
-    "Space",
-    (e) => {
-      spaceHeld.current = e.type === "keydown";
-    },
-    { keydown: true, keyup: true },
-    [],
-  );
-
-  useHotkeysDeprecated(
-    "meta, Ctrl",
-    (e) => {
-      controlHeld.current = e.ctrlKey || e.metaKey;
-    },
-    { keydown: true, keyup: true },
-    [],
-  );
-
-  const isControlHeld = () => controlHeld.current;
-  const isSpaceHeld = () => spaceHeld.current;
-  const isShiftHeld = () => shiftHeld.current;
+  const isControlHeld = () =>
+    isHotkeyPressed("ctrl") || isHotkeyPressed("meta"); //command
+  const isSpaceHeld = () => isHotkeyPressed("space");
+  const isShiftHeld = () => isHotkeyPressed("shift");
 
   return {
     isShiftHeld,
