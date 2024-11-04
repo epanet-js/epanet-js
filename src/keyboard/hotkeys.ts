d4352b5879d8af9e455f813862c3b12b5de4d18a
Sam Gathercole <sapagat@fastmail.com>
2024-11-04 14:29:04 +0100

    Use library functions to check if held

--- src/keyboard/hotkeys.ts
+++ src/keyboard/hotkeys.ts
@@ -1,10 +1,6 @@
 import Mousetrap from "mousetrap";
 import { useEffect } from "react";
-import { useHotkeys as rawUseHotkeys } from "react-hotkeys-hook";
 import { isDebugOn } from "src/infra/debug-mode";
-import { addToErrorLog } from "src/infra/error-tracking";
-
-type Params = Parameters<typeof rawUseHotkeys>;
 
 type DependencyList = ReadonlyArray<unknown>;
 
@@ -28,30 +24,3 @@ export const useHotkeys = (
     };
   }, dependencyList);
 };
-
-export function useHotkeysDeprecated(
-  keys: Params[0],
-  fn: Params[1],
-  a: Params[2],
-  b?: Params[3],
-) {
-  const wrap: Params[1] = (...args) => {
-    const message = typeof keys === "string" ? keys : keys.join(",");
-    addToErrorLog({
-      category: "keyboardshortcut",
-      message,
-      level: "info",
-    });
-    if (isDebugOn) {
-      // eslint-disable-next-line
-    console.log("HOTKEYS_CALL", keys, a, b);
-    }
-    return fn(...args);
-  };
-
-  if (isDebugOn) {
-    // eslint-disable-next-line
-    console.log("HOTKEYS_REGISTER", keys, a, b);
-  }
-  return rawUseHotkeys(keys, wrap, a, b);
-}
