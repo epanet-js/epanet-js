1c666b733f5a7fdfbe91c47773b064462391bb71
Sam Gathercole <sapagat@fastmail.com>
2024-11-05 14:10:25 +0100

    Extract hydraulic model test builder

---
+++ src/hydraulics/__helpers__/hydraulic-model-builder.ts
@@ -0,0 +1,53 @@
+import { Position } from "geojson";
+import {
+  AssetsMap,
+  NodeAsset,
+  createJunction,
+  createPipe,
+  getNodeCoordinates,
+} from "../assets";
+import { Topology } from "../topology";
+import { HydraulicModel } from "../hydraulic-model";
+
+export class HydraulicModelBuilder {
+  private topology: Topology;
+  private assets: AssetsMap;
+
+  static with() {
+    return new HydraulicModelBuilder();
+  }
+
+  constructor() {
+    this.assets = new Map();
+    this.topology = new Topology();
+  }
+
+  aNode(id: string, coordinates: Position) {
+    const node = createJunction(coordinates, id);
+    this.assets.set(id, node);
+    return this;
+  }
+
+  aLink(id: string, startNodeId: string, endNodeId: string) {
+    const startNode = this.assets.get(startNodeId);
+    const endNode = this.assets.get(endNodeId);
+    if (!startNode) throw new Error(`Start node (${startNodeId}) is missing`);
+    if (!endNode) throw new Error(`End node (${endNodeId}) is missing`);
+
+    const link = createPipe(
+      [
+        getNodeCoordinates(startNode as NodeAsset),
+        getNodeCoordinates(endNode as NodeAsset),
+      ],
+      id,
+    );
+    this.assets.set(link.id, link);
+    this.topology.addLink(id, startNodeId, endNodeId);
+
+    return this;
+  }
+
+  build(): HydraulicModel {
+    return { assets: this.assets, topology: this.topology };
+  }
+}
