9e3fefd1eb3a4c2868c7614e6a0e30e8012195fe
Sam Gathercole <sapagat@fastmail.com>
2024-11-07 06:13:16 +0100

    Allow to set debug mode from localstorage

--- src/infra/debug-mode.ts
+++ src/infra/debug-mode.ts
@@ -1,1 +1,8 @@
-export const isDebugOn = process.env.NEXT_PUBLIC_DEBUG_MODE === "true";
+const readFromLocalStorage = () => {
+  if (typeof window === "undefined") return false;
+
+  return localStorage.getItem("DEBUG_MODE") === "true";
+};
+
+export const isDebugOn =
+  process.env.NEXT_PUBLIC_DEBUG_MODE === "true" || readFromLocalStorage();
