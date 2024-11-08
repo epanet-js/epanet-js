8ce40ad00c7659950cf148dd3cb424c02f988e8e
Sam Gathercole <sapagat@fastmail.com>
2024-11-08 12:00:39 +0100

    Recover satellite mode

--- src/components/map_component.tsx
+++ src/components/map_component.tsx
@@ -256,17 +256,28 @@ export const MapComponent = memo(function MapComponent({
       dataUpdateInProgress.current = true;
 
       monitorFrequency("SET_MAP_DATA", { limit: 4, intervalMs: 1000 });
-      map.setOnlyData(data.hydraulicModel.assets).then(() => {
-        updateSelectionInMap();
-        updateEphemeralStateInMap();
-        dataUpdateInProgress.current = false;
-      });
+      map
+        .setOnlyStyle({
+          layerConfigs,
+          symbolization: symbolization || SYMBOLIZATION_NONE,
+          previewProperty: label,
+        })
+        .then(() => {
+          map.setOnlyData(data.hydraulicModel.assets).then(() => {
+            updateSelectionInMap();
+            updateEphemeralStateInMap();
+            dataUpdateInProgress.current = false;
+          });
+        });
     },
     [
       map,
       data.hydraulicModel.assets,
       updateSelectionInMap,
       updateEphemeralStateInMap,
+      layerConfigs,
+      symbolization,
+      label,
     ],
   );
 
