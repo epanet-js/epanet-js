9b774958285e98812e1876c5ff209e497d47583d
Sam Gathercole <sapagat@fastmail.com>
2024-11-12 15:37:11 +0100

    Refresh connections when applying moments

--- src/hydraulics/topology.test.ts
+++ src/hydraulics/topology.test.ts
@@ -17,11 +17,13 @@ describe("Topology", () => {
     const topology = new Topology();
 
     topology.addLink("link1", "A", "B");
+    topology.addLink("link2", "B", "C");
 
-    topology.removeNode("A");
+    topology.removeNode("B");
 
     expect(topology.getLinks("B")).toEqual([]);
     expect(topology.getLinks("A")).toEqual([]);
+    expect(topology.getLinks("C")).toEqual([]);
   });
 
   it("does not crash when removing missing node", () => {
@@ -41,5 +43,36 @@ describe("Topology", () => {
     topology.addLink("link2", "A", "B");
 
     expect(topology.getLinks("A")).toEqual(["link1", "link2"]);
+  });
+
+  it("crashes when trying to add two links with same id", () => {
+    const topology = new Topology();
+
+    topology.addLink("link1", "A", "B");
+
+    expect(() => {
+      topology.addLink("link1", "A", "B");
+    }).toThrow();
+
+    topology.removeNode("A");
+
+    topology.addLink("link1", "A", "B");
+
+    expect(topology.getLinks("A")).toEqual(["link1"]);
+  });
+
+  it("can remove a link by link data", () => {
+    const topology = new Topology();
+
+    topology.addLink("link1", "A", "B");
+    topology.addLink("link2", "A", "B");
+
+    topology.removeLink("link1");
+
+    expect(topology.getLinks("A")).toEqual(["link2"]);
+
+    topology.removeLink("link2");
+
+    expect(topology.getLinks("A")).toEqual([]);
   });
 });
