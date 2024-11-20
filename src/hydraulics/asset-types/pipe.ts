ea14495a39f0e92f334be42ec8657270fe968152
Sam Gathercole <sapagat@fastmail.com>
2024-11-20 15:28:30 +0100

    Assign default attributes to pipes

--- src/hydraulics/asset-types/pipe.ts
+++ src/hydraulics/asset-types/pipe.ts
@@ -2,32 +2,79 @@ import { Position } from "geojson";
 import { Link, LinkAttributes } from "./link";
 import { AssetId } from "./base-asset";
 import { newFeatureId } from "src/lib/id";
+import { Spec, canonicalize, getUnits, getValues } from "src/quantity";
 
 export type PipeAttributes = {
   type: "pipe";
   diameter: number;
+  roughnessHW: number;
+  roughnessDW: number;
+  roughnessCM: number;
 } & LinkAttributes;
+
+type PipeQuantities = Omit<
+  PipeAttributes,
+  "id" | "coordinates" | "type" | "connections" | "roughnessHW" | "roughnessCM"
+>;
+
+type HeadlossFormula = "H-W" | "D-W" | "C-M";
+type RoughnessKeys = "roughnessHW" | "roughnessDW" | "roughnessCM";
+const roughnessKeyFor: { [key in HeadlossFormula]: RoughnessKeys } = {
+  "H-W": "roughnessHW",
+  "D-W": "roughnessDW",
+  "C-M": "roughnessCM",
+};
+
+const canonicalSpec: Spec<PipeQuantities> = {
+  diameter: { value: 300, unit: "mm" },
+  length: { value: 1000, unit: "m" },
+  roughnessDW: { value: 0.26, unit: "mm" },
+};
+const canonicalDefaultValues = getValues(canonicalSpec);
+export const canonicalUnits = getUnits(canonicalSpec);
+
+export const usCustomaryDefaultValues = canonicalize(
+  {
+    diameter: { value: 12, unit: "in" },
+    length: { value: 1000, unit: "ft" },
+    roughnessDW: { value: 0.00085, unit: "ft" },
+  },
+  canonicalSpec,
+);
+
+const nullCoordinates = [
+  [0, 0],
+  [0, 0],
+];
+
+type BuildData = {
+  id?: AssetId;
+  coordinates?: Position[];
+} & Partial<PipeAttributes>;
 
 export class Pipe extends Link<PipeAttributes> {
   static build({
     id = newFeatureId(),
-    coordinates = [
-      [0, 0],
-      [0, 0],
-    ],
-    diameter = 0,
-    length = 0,
+    coordinates = nullCoordinates,
     connections = ["", ""],
-  }: {
-    id?: AssetId;
-    coordinates?: Position[];
-  } & Partial<PipeAttributes> = {}) {
-    return new Pipe(id, coordinates, {
+    diameter,
+    length,
+    roughnessHW = 130,
+    roughnessDW,
+    roughnessCM = 0.012,
+  }: BuildData = {}) {
+    const defaultValues = canonicalDefaultValues;
+    const attributes: PipeAttributes = {
       type: "pipe",
-      diameter,
-      length,
-      connections,
-    });
+      diameter: diameter ?? defaultValues.diameter,
+      length: length ?? defaultValues.length,
+      roughnessHW: roughnessHW ?? defaultValues.roughnessHW,
+      roughnessDW: roughnessDW ?? defaultValues.roughnessDW,
+      roughnessCM: roughnessCM ?? defaultValues.roughnessCM,
+      connections: connections,
+    };
+
+    return new Pipe(id, coordinates, attributes);
   }
 
   get diameter() {
@@ -36,6 +83,14 @@ export class Pipe extends Link<PipeAttributes> {
 
   setDiameter(value: number) {
     this.attributes.diameter = value;
+  }
+
+  roughnessFor(headlossFormula: HeadlossFormula) {
+    return this.attributes[roughnessKeyFor[headlossFormula]];
+  }
+
+  setRoughness(value: number, headlossFormula: HeadlossFormula) {
+    this.attributes[roughnessKeyFor[headlossFormula]] = value;
   }
 
   copy() {
