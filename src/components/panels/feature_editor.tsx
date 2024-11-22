a28cb1595e41d3d10141531a15afc1df3da1ff80
Sam Gathercole <sapagat@fastmail.com>
2024-11-22 14:18:13 +0100

    Remove FLAG_DEFAULTS

--- src/components/panels/feature_editor.tsx
+++ src/components/panels/feature_editor.tsx
@@ -1,11 +1,9 @@
 import { useAtomValue } from "jotai";
-import { FeatureEditorInnerDeprecated } from "./feature_editor/feature_editor_inner_deprecated";
 import { FeatureEditorInner } from "./feature_editor/feature_editor_inner";
 import FeatureEditorMulti from "./feature_editor/feature_editor_multi";
 import React from "react";
 import { NothingSelected } from "src/components/nothing_selected";
 import { selectedFeaturesAtom } from "src/state/jotai";
-import { isFeatureOn } from "src/infra/feature-flags";
 
 export default function FeatureEditor() {
   const selectedFeatures = useAtomValue(selectedFeaturesAtom);
@@ -14,11 +12,7 @@ export default function FeatureEditor() {
     selectedFeatures.length > 1 ? (
       <FeatureEditorMulti selectedFeatures={selectedFeatures} />
     ) : selectedFeatures.length === 1 ? (
-      isFeatureOn("FLAG_DEFAULTS") ? (
-        <FeatureEditorInner selectedFeature={selectedFeatures[0]} />
-      ) : (
-        <FeatureEditorInnerDeprecated selectedFeature={selectedFeatures[0]} />
-      )
+      <FeatureEditorInner selectedFeature={selectedFeatures[0]} />
     ) : (
       <NothingSelected />
     );
