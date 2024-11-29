1d2c70ea37a59abdcccc4b494f34a8d6ebaa7de0
Sam Gathercole <sapagat@fastmail.com>
2024-11-29 15:37:01 +0100

    Refactor build inp function

--- src/simulation/build-inp.test.ts
+++ src/simulation/build-inp.test.ts
@@ -4,43 +4,63 @@ import { HydraulicModel, Junction, Pipe, Reservoir } from "src/hydraulic-model";
 type SimulationPipeStatus = "Open" | "Closed";
 
 const buildInp = (hydraulicModel: HydraulicModel): string => {
+  const defaultUnits = "LPS";
+  const defaultHeadloss = "H-W";
+  const defaultMinorloss = 0;
+  const oneStep = 0;
   const sections = {
-    reservoirs: `[RESERVOIR]\n;id\thead\tpattern\n`,
-    junctions: `[JUNCTIONS]\n;id\televation\n`,
-    demands: `[DEMANDS]\n;id\tdemand\tpattern\tcategory\n`,
-    pipes: `[PIPES]\n;id\tstart\tend\tlength\tdiameter\troughness\tminorLoss\tstatus\n`,
-    report: `[REPORT]\nStatus\tFULL\n`,
-    times: `[TIMES]\nDuration\t0\n`,
-    options: `[OPTIONS]\nUnits\tLPS\nHeadloss\tH-W\n`,
+    junctions: ["[JUNCTIONS]", ";id\televation"],
+    reservoirs: ["[RESERVOIR]", ";id\thead\tpattern"],
+    pipes: [
+      "[PIPES]",
+      ";id\tstart\tend\tlength\tdiameter\troughness\tminorLoss\tstatus",
+    ],
+    demands: ["[DEMANDS]", ";id\tdemand\tpattern\tcategory"],
+    times: ["[TIMES]", `Duration\t${oneStep}`],
+    report: ["[REPORT]", "Status\tFULL"],
+    options: [
+      "[OPTIONS]",
+      `Units\t${defaultUnits}`,
+      `Headloss\t${defaultHeadloss}`,
+    ],
   };
 
   for (const asset of hydraulicModel.assets.values()) {
     if (asset.type === "reservoir") {
       const reservoir = asset as Reservoir;
-      sections.reservoirs += `${reservoir.id}\t${reservoir.head}\n`;
+      sections.reservoirs.push([reservoir.id, reservoir.head].join("\t"));
     }
     if (asset.type === "junction") {
       const junction = asset as Junction;
-      sections.junctions += `${junction.id}\t${junction.elevation}\n`;
-      sections.demands += `${junction.id}\t${junction.demand}\n`;
+      sections.junctions.push([junction.id, junction.elevation].join("\t"));
+      sections.demands.push([junction.id, junction.demand].join("\t"));
     }
     if (asset.type === "pipe") {
       const pipe = asset as Pipe;
       const [nodeStart, nodeEnd] = pipe.connections;
-      const minorLoss = 0;
-      const status = pipeStatusFor(pipe);
-      sections.pipes += `${pipe.id}\t${nodeStart}\t${nodeEnd}\t${pipe.length}\t${pipe.diameter}\t${pipe.roughness}\t${minorLoss}\t${status}\n`;
+      sections.pipes.push(
+        [
+          pipe.id,
+          nodeStart,
+          nodeEnd,
+          pipe.length,
+          pipe.diameter,
+          pipe.roughness,
+          defaultMinorloss,
+          pipeStatusFor(pipe),
+        ].join("\t"),
+      );
     }
   }
 
   return [
-    sections.junctions,
-    sections.reservoirs,
-    sections.pipes,
-    sections.demands,
-    sections.times,
-    sections.report,
-    sections.options,
+    sections.junctions.join("\n"),
+    sections.reservoirs.join("\n"),
+    sections.pipes.join("\n"),
+    sections.demands.join("\n"),
+    sections.times.join("\n"),
+    sections.report.join("\n"),
+    sections.options.join("\n"),
     "[END]",
   ].join("\n");
 };
