7a78e20a3d21a61c8bba37184849f4abb5a63f07
Sam Gathercole <sapagat@fastmail.com>
2024-12-02 17:41:07 +0100

    Expose function to run epanet simulation

---
+++ src/simulation/epanet-simulation.test.ts
@@ -0,0 +1,32 @@
+import { HydraulicModelBuilder } from "src/__helpers__/hydraulic-model-builder";
+import { buildInp } from "./build-inp";
+import { runSimulation } from "./epanet-simulation";
+
+describe("epanet simulation", () => {
+  it("includes a report", () => {
+    const hydraulicModel = HydraulicModelBuilder.with()
+      .aReservoir("r1")
+      .aJunction("j1")
+      .aPipe("p1", "r1", "j1")
+      .build();
+    const inp = buildInp(hydraulicModel);
+
+    const { status, report } = runSimulation(inp);
+
+    expect(status).toEqual("success");
+    expect(report).not.toContain("Error");
+  });
+
+  it("reports says when simulation fails", () => {
+    const hydraulicModel = HydraulicModelBuilder.with()
+      .aReservoir("r1")
+      .aReservoir("r2")
+      .aPipe("p1", "r1", "r2", {})
+      .build();
+    const inp = buildInp(hydraulicModel);
+    const { status, report } = runSimulation(inp);
+
+    expect(status).toEqual("failure");
+    expect(report).toContain("Error 223: not enough nodes");
+  });
+});
