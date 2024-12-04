1e93b60423fa777e62057c888eaeb229fa18a2f3
Sam Gathercole <sapagat@fastmail.com>
2024-12-04 13:01:31 +0100

    Ensure epanet error only appears once

--- src/simulation/epanet-simulation.test.ts
+++ src/simulation/epanet-simulation.test.ts
@@ -29,4 +29,20 @@ describe("epanet simulation", () => {
     expect(status).toEqual("failure");
     expect(report).toContain("Error 223: not enough nodes");
   });
+
+  it("can include multiple errors in the report", () => {
+    const hydraulicModel = HydraulicModelBuilder.with()
+      .aReservoir("r1")
+      .aJunction("j1")
+      .aPipe("p1", "r1", "j1")
+      .aJunction("j2")
+      .build();
+    const inp = buildInp(hydraulicModel);
+    const { status, report } = runSimulation(inp);
+
+    expect(status).toEqual("failure");
+    expect(report.match(/Error 233/gi)!.length).toEqual(1);
+    expect(report).toContain("j2");
+    expect(report).toContain("Error 200");
+  });
 });
