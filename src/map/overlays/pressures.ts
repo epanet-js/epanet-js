3e6209e9fe21aedcf1095572ff30bb30c377079f
Sam Gathercole <sapagat@fastmail.com>
2024-12-09 18:29:17 +0100

    Extract logic to build pressures overlay

---
+++ src/map/overlays/pressures.ts
@@ -0,0 +1,48 @@
+import { ScatterplotLayer } from "@deck.gl/layers";
+import { AssetId, AssetsMap, Junction } from "src/hydraulic-model";
+import { ISymbolizationRamp } from "src/types";
+
+export const buildPressuresOverlay = (
+  assets: AssetsMap,
+  symbolization: ISymbolizationRamp,
+  skipSet: Set<AssetId>,
+) => {
+  const steps = symbolization.stops.map((stop) => {
+    return {
+      value: stop.input,
+      color: stop.output
+        .replace("rgb(", "")
+        .replace(")", "")
+        .split(",")
+        .map((value) => parseInt(value.trim())),
+    };
+  });
+
+  const colorFor = (value: number): number[] => {
+    const step = steps.find((step) => value <= step.value);
+    return step ? step.color : steps[steps.length - 1].color;
+  };
+
+  const data = [];
+  for (const asset of assets.values()) {
+    if (asset.type !== "junction" || skipSet.has(asset.id)) continue;
+
+    data.push({
+      color: colorFor((asset as Junction).pressure || 0),
+      coordinates: asset.coordinates,
+    });
+  }
+
+  return [
+    new ScatterplotLayer({
+      id: "analysis-pressures",
+      data,
+      getPosition: (d) => d.coordinates as [number, number],
+      getRadius: 6,
+      radiusUnits: "pixels",
+      getFillColor: (d) => d.color as [number, number, number],
+      pickable: false,
+      antialiasing: true,
+    }),
+  ];
+};
