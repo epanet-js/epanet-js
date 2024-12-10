c4e8157348da88529855965cb387db2d7f6101cc
Sam Gathercole <sapagat@fastmail.com>
2024-12-10 16:58:01 +0100

    Switch to asset tab when selecting asset after analysis

--- src/selection/use-selection.ts
+++ src/selection/use-selection.ts
@@ -1,17 +1,21 @@
 import { useSetAtom } from "jotai";
 import { USelection } from "./selection";
-import { Sel, selectionAtom } from "src/state/jotai";
+import { Sel, TabOption, selectionAtom, tabAtom } from "src/state/jotai";
 import { IWrappedFeature } from "src/types";
+import { isFeatureOn } from "src/infra/feature-flags";
 
 export const useSelection = (selection: Sel) => {
   const setSelection = useSetAtom(selectionAtom);
+  const setTab = useSetAtom(tabAtom);
 
   const toggleSingleSelection = (featureId: IWrappedFeature["id"]) => {
     setSelection(USelection.toggleSingleSelectionId(selection, featureId));
+    isFeatureOn("FLAG_PRESSURES") && setTab(TabOption.Asset);
   };
 
   const extendSelection = (featureId: IWrappedFeature["id"]) => {
     setSelection(USelection.addSelectionId(selection, featureId));
+    isFeatureOn("FLAG_PRESSURES") && setTab(TabOption.Asset);
   };
 
   const isSelected = (featureId: IWrappedFeature["id"]) => {
@@ -20,6 +24,7 @@ export const useSelection = (selection: Sel) => {
 
   const selectFeature = (featureId: IWrappedFeature["id"]) => {
     setSelection(USelection.single(featureId));
+    isFeatureOn("FLAG_PRESSURES") && setTab(TabOption.Asset);
   };
 
   const removeFromSelection = (featureId: IWrappedFeature["id"]) => {
