0d259c75dc9745f3b240beec136d0334383945bf
Sam Gathercole <sapagat@fastmail.com>
2024-12-11 12:39:17 +0100

    Extract analysis types to its own module

--- src/state/analysis.ts
+++ src/state/analysis.ts
@@ -1,18 +1,5 @@
 import { atom } from "jotai";
-import { RangeColorMapping } from "src/analysis/range-color-mapping";
-
-export type PressuresAnalysis = {
-  type: "pressures";
-  rangeColorMapping: RangeColorMapping;
-};
-
-export type FlowsAnalysis = {
-  type: "flows";
-  rangeColorMapping: RangeColorMapping;
-};
-
-export type NodesAnalysis = { type: "none" } | PressuresAnalysis;
-export type LinksAnalysis = { type: "none" } | FlowsAnalysis;
+import { LinksAnalysis, NodesAnalysis } from "src/analysis";
 
 export type AnalysisState = {
   nodes: NodesAnalysis;
