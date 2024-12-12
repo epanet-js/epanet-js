2fa619ac5c0f172baf87faa9854f5178bf6f4e60
Sam Gathercole <sapagat@fastmail.com>
2024-12-12 10:23:10 +0100

    Display arrow in the middle of pipes

--- src/map/overlays/flows.ts
+++ src/map/overlays/flows.ts
@@ -1,15 +1,20 @@
-import { GeoJsonLayer } from "@deck.gl/layers";
+import { GeoJsonLayer, IconLayer } from "@deck.gl/layers";
 import { RangeColorMapping } from "src/analysis/range-color-mapping";
 import { AssetId, AssetsMap, Pipe } from "src/hydraulic-model";
-import { IFeature } from "src/types";
+import { IFeature, Position } from "src/types";
 import { slots } from "../slots";
+import turfMidpont from "@turf/midpoint";
+import { bearing } from "@turf/bearing";
+import { getIconsSprite } from "../icons";
 
 export const buildFlowsOverlay = (
   assets: AssetsMap,
   rangeColorMapping: RangeColorMapping,
   conditionFn: (assetId: AssetId) => boolean,
 ) => {
+  const iconsSprite = getIconsSprite();
   const data: Pipe[] = [];
+  const arrows = [];
   for (const asset of assets.values()) {
     if (
       asset.type !== "pipe" ||
@@ -18,6 +23,7 @@ export const buildFlowsOverlay = (
     )
       continue;
 
+    arrows.push(...calculateArrows(asset as Pipe));
     data.push(asset as Pipe);
   }
 
@@ -38,5 +44,39 @@ export const buildFlowsOverlay = (
       lineCapRounded: true,
       antialiasing: true,
     }),
+    new IconLayer({
+      id: "analysis-flows-icons",
+      data: arrows,
+      getSize: 20,
+      sizeUnits: "meters",
+      sizeMinPixels: 4,
+      sizeMaxPixels: 20,
+
+      // @ts-expect-error type should be allowed https://deck.gl/docs/api-reference/layers/icon-layer#iconatlas
+      iconAtlas: iconsSprite.atlas,
+      iconMapping: iconsSprite.mapping,
+      getIcon: (_d) => "arrow-white",
+      getAngle: (d) => d.angle,
+      getPosition: (d) => d.coordinates,
+    }),
   ];
+};
+
+const calculateArrows = (
+  pipe: Pipe,
+): { angle: number; coordinates: Position }[] => {
+  const coordinates = pipe.coordinates;
+  const midpoints = [];
+  for (let i = 0; i < coordinates.length - 1; i++) {
+    const start = coordinates[i];
+    const end = coordinates[i + 1];
+    const angleFromNorth = bearing(start, end);
+    const angleFromEast = (90 - angleFromNorth + 360) % 360;
+
+    midpoints.push({
+      coordinates: turfMidpont(start, end).geometry.coordinates,
+      angle: angleFromEast,
+    });
+  }
+  return midpoints;
 };
