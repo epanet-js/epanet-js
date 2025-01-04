18e6345d46fe38b3eefc5d4c1b805b2696f85ffa
Sam Gathercole <sapagat@fastmail.com>
2025-01-04 13:34:53 +0100

    Rename feature editor inner to asset editor

--- src/components/panels/AssetEditor.tsx
+++ src/components/panels/AssetEditor.tsx
@@ -1,12 +1,29 @@
 import type { IWrappedFeature } from "src/types";
-import { FeatureEditorProperties } from "./feature_editor_properties";
-import { FeatureEditorId } from "./feature_editor_id";
-import React, { useCallback, useMemo, useRef } from "react";
-import { RawEditor } from "./raw_editor";
+import { FeatureEditorProperties } from "./feature_editor/feature_editor_properties";
+import { FeatureEditorId } from "./feature_editor/feature_editor_id";
+import React, {
+  ChangeEventHandler,
+  FocusEventHandler,
+  KeyboardEventHandler,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { RawEditor } from "./feature_editor/raw_editor";
 import { Asset, AssetStatus, Junction, Pipe } from "src/hydraulic-model";
 import { PanelDetails } from "src/components/panel_details";
-import { localizeDecimal, translate, translateUnit } from "src/infra/i18n";
-import { PropertyRow, PropertyRowReadonly } from "./property_row";
+import {
+  localizeDecimal,
+  parseLocaleNumber,
+  reformatWithoutGroups,
+  translate,
+  translateUnit,
+} from "src/infra/i18n";
+import {
+  PropertyRow,
+  PropertyRowReadonly,
+} from "./feature_editor/property_row";
 import { isDebugOn } from "src/infra/debug-mode";
 import { Unit } from "src/quantity";
 
@@ -22,9 +39,11 @@ import {
 import { useAtomValue } from "jotai";
 import { dataAtom } from "src/state/jotai";
 import { usePersistence } from "src/lib/persistence/context";
-import { NumericField, Selector } from "./property_row/value";
+import * as E from "src/components/elements";
+import * as Select from "@radix-ui/react-select";
+import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons";
 
-export function FeatureEditorInner({
+export function AssetEditor({
   selectedFeature,
   quantitiesMetadata,
 }: {
@@ -35,7 +54,7 @@ export function FeatureEditorInner({
     <>
       <div className="flex-auto overflow-y-auto placemark-scrollbar">
         {selectedFeature instanceof BaseAsset ? (
-          <AssetEditor
+          <AssetEditorInner
             asset={selectedFeature as Asset}
             quantitiesMetadata={quantitiesMetadata}
           />
@@ -55,7 +74,7 @@ export function FeatureEditorInner({
   );
 }
 
-export const AssetEditor = ({
+const AssetEditorInner = ({
   asset,
   quantitiesMetadata,
 }: {
@@ -445,3 +464,160 @@ export function PropertyTableHead() {
     </thead>
   );
 }
+
+const NumericField = ({
+  label,
+  displayValue,
+  onChangeValue,
+  readOnly = false,
+}: {
+  label: string;
+  displayValue: string;
+  onChangeValue: (newValue: number) => void;
+  readOnly?: boolean;
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputValue, setInputValue] = useState(displayValue);
+  const [hasError, setError] = useState(false);
+  const [isDirty, setDirty] = useState(false);
+
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === "Enter" || e.key === "Escape") {
+      handleCommitLastChange();
+      return;
+    }
+    if ((e.ctrlKey || e.metaKey) && (e.key === "z" || e.key === "y")) {
+      e.preventDefault();
+    }
+  };
+
+  const handleBlur = () => {
+    if (isDirty) {
+      handleCommitLastChange();
+    } else {
+      setInputValue(displayValue);
+    }
+  };
+
+  const handleFocus: FocusEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    setInputValue(reformatWithoutGroups(displayValue));
+    setTimeout(() => inputRef.current && inputRef.current.select(), 0);
+  };
+
+  const handleCommitLastChange = () => {
+    const numericValue = parseLocaleNumber(inputValue);
+    if (isNaN(numericValue)) {
+      setInputValue(displayValue);
+    } else {
+      setInputValue(String(numericValue));
+      onChangeValue(numericValue);
+    }
+
+    setDirty(false);
+    setError(false);
+    setTimeout(() => inputRef.current && inputRef.current.blur(), 0);
+  };
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const newInputValue = e.target.value;
+    setInputValue(newInputValue);
+    const numericValue = parseLocaleNumber(newInputValue);
+    setError(isNaN(numericValue));
+    setDirty(true);
+  };
+
+  if (hasError && inputRef.current) {
+    inputRef.current.className = E.styledPropertyInputWithError("right");
+  }
+  if (!hasError && inputRef.current) {
+    inputRef.current.className = E.styledPropertyInput("right");
+  }
+
+  return (
+    <div className="relative group-1">
+      <input
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        spellCheck="false"
+        type="text"
+        className={E.styledPropertyInput("right")}
+        aria-label={`Value for: ${label}`}
+        readOnly={readOnly}
+        onBlur={handleBlur}
+        ref={inputRef}
+        value={inputValue}
+        onFocus={handleFocus}
+        tabIndex={1}
+      />
+    </div>
+  );
+};
+
+const Selector = <T extends string>({
+  options,
+  selected,
+  onChange,
+}: {
+  options: { label: string; value: T }[];
+  selected: { label: string; value: T };
+  onChange: (selected: T) => void;
+}) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpenChange = (isOpen: boolean) => {
+    setOpen(isOpen);
+  };
+
+  const handleKeyDown: KeyboardEventHandler<HTMLDivElement> = (event) => {
+    if (event.code === "Escape" || event.code === "Enter") {
+      event.stopPropagation();
+      setOpen(false);
+    }
+  };
+
+  return (
+    <div className="relative group-1">
+      <Select.Root
+        value={selected.value}
+        open={isOpen}
+        onOpenChange={handleOpenChange}
+        onValueChange={onChange}
+      >
+        <Select.Trigger
+          aria-label={`Value for: Status`}
+          tabIndex={1}
+          className="flex items-center text-xs text-gray-700 dark:items-center justify-between w-full min-w-[90px] pr-1 pl-2 pl-min-2 py-2 focus:ring-inset focus:ring-1 focus:ring-purple-500 focus:bg-purple-300/10"
+        >
+          <Select.Value />
+          <Select.Icon className="px-1">
+            <ChevronDownIcon />
+          </Select.Icon>
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content
+            onKeyDown={handleKeyDown}
+            onCloseAutoFocus={(e) => e.preventDefault()}
+            className="bg-white w-full border text-xs rounded-md shadow-md"
+          >
+            <Select.Viewport className="p-1">
+              {options.map((option, i) => (
+                <Select.Item
+                  key={i}
+                  value={option.value}
+                  className="flex items-center px-2 py-2 cursor-pointer focus:bg-purple-300/40"
+                >
+                  <Select.ItemText>{option.label}</Select.ItemText>
+                  <Select.ItemIndicator className="ml-auto">
+                    <CheckIcon className="text-purple-700" />
+                  </Select.ItemIndicator>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+    </div>
+  );
+};
