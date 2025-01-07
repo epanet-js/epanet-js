bd8ad6e531e3eeffdb032c662faccc2c63cbfe67
Sam Gathercole <sapagat@fastmail.com>
2025-01-07 19:20:48 +0100

    Make read only multi asset editor

--- src/components/panels/feature_editor/feature_editor_multi.tsx
+++ src/components/panels/feature_editor/feature_editor_multi.tsx
@@ -1,5 +1,9 @@
 import { IWrappedFeature } from "src/types";
-import { FeatureEditorPropertiesMulti } from "./feature_editor_properties_multi";
+import {
+  FeatureEditorPropertiesMulti,
+  FeatureEditorPropertiesMultiDeprecated,
+} from "./feature_editor_properties_multi";
+import { isFeatureOn } from "src/infra/feature-flags";
 
 export default function FeatureEditorMulti({
   selectedFeatures,
@@ -9,7 +13,14 @@ export default function FeatureEditorMulti({
   return (
     <>
       <div className="overflow-auto">
-        <FeatureEditorPropertiesMulti selectedFeatures={selectedFeatures} />
+        {isFeatureOn("FLAG_MULTI_ASSETS") && (
+          <FeatureEditorPropertiesMulti selectedFeatures={selectedFeatures} />
+        )}
+        {!isFeatureOn("FLAG_MULTI_ASSETS") && (
+          <FeatureEditorPropertiesMultiDeprecated
+            selectedFeatures={selectedFeatures}
+          />
+        )}
       </div>
       <div className="flex-auto" />
       <div className="divide-y divide-gray-200 dark:divide-gray-900 border-t border-gray-200 dark:border-gray-900 overflow-auto placemark-scrollbar"></div>
