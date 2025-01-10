7823207041261487f289463461f7dbe7d61cf783
Sam Gathercole <sapagat@fastmail.com>
2025-01-10 14:01:18 +0100

    Compute times of each value

--- src/components/panels/selection-stats.test.ts
+++ src/components/panels/selection-stats.test.ts
@@ -8,60 +8,79 @@ type QuantityStats = {
   max: number;
   min: number;
   mean: number;
-  values: number[];
+  values: Map<number, number>;
 };
 
 const computeStats = (assets: Asset[]): Map<string, QuantityStats> => {
-  const property = "elevation";
   const statsMap = new Map<string, QuantityStats>();
   for (const asset of assets) {
-    if (!asset.hasProperty(property)) continue;
-    if (!statsMap.has(property)) {
-      statsMap.set(property, {
-        type: "quantity",
-        property,
-        sum: 0,
-        min: Infinity,
-        max: -Infinity,
-        mean: 0,
-        values: [],
-      });
+    const properties = asset.listProperties();
+    for (const property of properties) {
+      const value = asset.getProperty(property) as unknown as number;
+      if (!statsMap.has(property)) {
+        statsMap.set(property, {
+          type: "quantity",
+          property,
+          sum: 0,
+          min: Infinity,
+          max: -Infinity,
+          mean: 0,
+          values: new Map(),
+        });
+      }
+
+      const propertyStats = statsMap.get(property) as QuantityStats;
+
+      if (value < propertyStats.min) propertyStats.min = value;
+      if (value > propertyStats.max) propertyStats.max = value;
+
+      propertyStats.sum += value;
+
+      propertyStats.values.set(
+        value,
+        (propertyStats.values.get(value) || 0) + 1,
+      );
+
+      propertyStats.mean = propertyStats.sum / propertyStats.values.size;
     }
-
-    const propertyStats = statsMap.get(property) as QuantityStats;
-    const value = asset.getProperty(property) as unknown as number;
-
-    if (value < propertyStats.min) propertyStats.min = value;
-    if (value > propertyStats.max) propertyStats.max = value;
-
-    propertyStats.sum += value;
-    propertyStats.values.push(value);
-
-    propertyStats.mean = propertyStats.sum / propertyStats.values.length;
   }
 
   return statsMap;
 };
 
 describe("Selection stats", () => {
-  it("computes stats of a property", () => {
+  it("can compute stats of a quantity", () => {
     const { assets } = HydraulicModelBuilder.with()
       .aJunction("J1", { elevation: 10 })
       .aJunction("J2", { elevation: 20 })
+      .aJunction("J3", { elevation: 20 })
+      .aReservoir("R1", { elevation: 30 })
+      .build();
+    const selection = [...assets.values()];
+
+    const statsMap = computeStats(selection);
+    const propertyStats = statsMap.get("elevation") as QuantityStats;
+
+    expect(propertyStats.type).toEqual("quantity");
+    expect(propertyStats.property).toEqual("elevation");
+    expect(propertyStats.sum).toEqual(80);
+    expect(propertyStats.min).toEqual(10);
+    expect(propertyStats.max).toEqual(30);
+    expect(propertyStats.mean).toBeCloseTo(26.666);
+    expect(propertyStats.values.get(10)).toEqual(1);
+    expect(propertyStats.values.get(20)).toEqual(2);
+  });
+
+  it("can compute multiple properties", () => {
+    const { assets } = HydraulicModelBuilder.with()
+      .aJunction("J1", { elevation: 10, demand: 20 })
       .aReservoir("R1", { elevation: 30 })
       .build();
     const selection = [...assets.values()];
 
     const statsMap = computeStats(selection);
 
-    const propertyStats = statsMap.get("elevation") as QuantityStats;
-
-    expect(propertyStats.type).toEqual("quantity");
-    expect(propertyStats.property).toEqual("elevation");
-    expect(propertyStats.values).toEqual([10, 20, 30]);
-    expect(propertyStats.sum).toEqual(60);
-    expect(propertyStats.min).toEqual(10);
-    expect(propertyStats.max).toEqual(30);
-    expect(propertyStats.mean).toEqual(20);
+    expect(statsMap.get("demand")!.values.get(20)).toEqual(1);
+    expect([...statsMap.get("elevation")!.values.keys()]).toEqual([10, 30]);
   });
 });
