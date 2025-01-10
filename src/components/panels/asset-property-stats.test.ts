77ed18104aee80e031cb2ec1bc20d7e53a587b47
Sam Gathercole <sapagat@fastmail.com>
2025-01-10 14:25:38 +0100

    Compute state for category properties

---
+++ src/components/panels/asset-property-stats.test.ts
@@ -0,0 +1,61 @@
+import { HydraulicModelBuilder } from "src/__helpers__/hydraulic-model-builder";
+import {
+  CategoryStats,
+  QuantityStats,
+  computePropertyStats,
+} from "./asset-property-stats";
+
+describe("Asset property stats", () => {
+  it("can compute stats of a quantity", () => {
+    const { assets } = HydraulicModelBuilder.with()
+      .aJunction("J1", { elevation: 10 })
+      .aJunction("J2", { elevation: 20 })
+      .aJunction("J3", { elevation: 20 })
+      .aReservoir("R1", { elevation: 30 })
+      .build();
+    const selection = [...assets.values()];
+
+    const statsMap = computePropertyStats(selection);
+    const propertyStats = statsMap.get("elevation") as QuantityStats;
+
+    expect(propertyStats.type).toEqual("quantity");
+    expect(propertyStats.property).toEqual("elevation");
+    expect(propertyStats.sum).toEqual(80);
+    expect(propertyStats.min).toEqual(10);
+    expect(propertyStats.max).toEqual(30);
+    expect(propertyStats.mean).toBeCloseTo(26.666);
+    expect(propertyStats.values.get(10)).toEqual(1);
+    expect(propertyStats.values.get(20)).toEqual(2);
+  });
+
+  it("can compute stats for categories", () => {
+    const { assets } = HydraulicModelBuilder.with()
+      .aJunction("J1")
+      .aJunction("J2")
+      .aPipe("J3")
+      .aReservoir("R1")
+      .build();
+    const selection = [...assets.values()];
+
+    const statsMap = computePropertyStats(selection);
+    const propertyStats = statsMap.get("type") as CategoryStats;
+
+    expect(propertyStats.type).toEqual("category");
+    expect(propertyStats.values.get("junction")).toEqual(2);
+    expect(propertyStats.values.get("pipe")).toEqual(1);
+    expect(propertyStats.values.get("reservoir")).toEqual(1);
+  });
+
+  it("can compute multiple properties", () => {
+    const { assets } = HydraulicModelBuilder.with()
+      .aJunction("J1", { elevation: 10, demand: 20 })
+      .aReservoir("R1", { elevation: 30 })
+      .build();
+    const selection = [...assets.values()];
+
+    const statsMap = computePropertyStats(selection);
+
+    expect((statsMap.get("demand") as QuantityStats).values.get(20)).toEqual(1);
+    expect([...statsMap.get("elevation")!.values.keys()]).toEqual([10, 30]);
+  });
+});
---
+++ src/components/panels/asset-property-stats.ts
@@ -0,0 +1,82 @@
+import { Asset } from "src/hydraulic-model";
+
+export type QuantityStats = {
+  type: "quantity";
+  property: string;
+  sum: number;
+  max: number;
+  min: number;
+  mean: number;
+  values: Map<number, number>;
+};
+
+export type CategoryStats = {
+  type: "category";
+  property: string;
+  values: Map<string, number>;
+};
+
+type StatsMap = Map<string, QuantityStats | CategoryStats>;
+
+export const computePropertyStats = (assets: Asset[]): StatsMap => {
+  const statsMap = new Map();
+  for (const asset of assets) {
+    const properties = asset.listProperties();
+    for (const property of properties) {
+      const value = asset.getProperty(property) as unknown as number;
+      if (typeof value === "string") {
+        updateCategoryStats(statsMap, property, value);
+      } else {
+        updateQuantityStats(statsMap, property, value);
+      }
+    }
+  }
+
+  return statsMap;
+};
+
+const updateQuantityStats = (
+  statsMap: StatsMap,
+  property: string,
+  value: number,
+) => {
+  if (!statsMap.has(property)) {
+    statsMap.set(property, {
+      type: "quantity",
+      property,
+      sum: 0,
+      min: Infinity,
+      max: -Infinity,
+      mean: 0,
+      values: new Map(),
+    });
+  }
+
+  const propertyStats = statsMap.get(property) as QuantityStats;
+
+  if (value < propertyStats.min) propertyStats.min = value;
+  if (value > propertyStats.max) propertyStats.max = value;
+
+  propertyStats.sum += value;
+
+  propertyStats.values.set(value, (propertyStats.values.get(value) || 0) + 1);
+
+  propertyStats.mean = propertyStats.sum / propertyStats.values.size;
+};
+
+const updateCategoryStats = (
+  statsMap: StatsMap,
+  property: string,
+  value: string,
+) => {
+  if (!statsMap.has(property)) {
+    statsMap.set(property, {
+      type: "category",
+      property,
+      values: new Map(),
+    });
+  }
+
+  const propertyStats = statsMap.get(property) as CategoryStats;
+  propertyStats.values.set(value, (propertyStats.values.get(value) || 0) + 1);
+};
