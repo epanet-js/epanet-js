2db736f894cf9776d86f0ad068107332e862497c
Sam Gathercole <sapagat@fastmail.com>
2025-01-17 10:51:47 +0100

    Remove FLAG_CLOSED_PIPES

--- src/map/layers/pipes.ts
+++ src/map/layers/pipes.ts
@@ -4,7 +4,6 @@ import { ISymbolization } from "src/types";
 import { asColorExpression, asNumberExpression } from "src/lib/symbolization";
 import { DataSource } from "../data-source";
 import { LayerId } from "./layer";
-import { isFeatureOn } from "src/infra/feature-flags";
 
 export const pipesLayer = ({
   source,
@@ -15,49 +14,30 @@ export const pipesLayer = ({
   layerId: LayerId;
   symbolization: ISymbolization;
 }): LineLayer => {
-  const paint = isFeatureOn("FLAG_CLOSED_PIPES")
-    ? {
-        "line-opacity": [
-          "case",
-          ["boolean", ["feature-state", "hidden"], false],
-          0,
-          asNumberExpression({
-            symbolization,
-            part: "stroke-opacity",
-            defaultValue: 1,
-          }),
-        ],
-        "line-width": ["interpolate", ["linear"], ["zoom"], 12, 0.5, 16, 4],
-        "line-color": handleSelected(
-          asColorExpression({ symbolization, part: "stroke" }),
-          false,
-          LINE_COLORS_SELECTED,
-        ),
-        "line-dasharray": [
-          "case",
-          ["==", ["get", "status"], "closed"],
-          ["literal", [2, 1]],
-          ["literal", [1, 0]],
-        ],
-      }
-    : {
-        "line-opacity": [
-          "case",
-          ["boolean", ["feature-state", "hidden"], false],
-          0,
-          asNumberExpression({
-            symbolization,
-            part: "stroke-opacity",
-            defaultValue: 1,
-          }),
-        ],
-        "line-width": ["interpolate", ["linear"], ["zoom"], 12, 0.5, 16, 4],
-        "line-color": handleSelected(
-          asColorExpression({ symbolization, part: "stroke" }),
-          false,
-          LINE_COLORS_SELECTED,
-        ),
-      };
+  const paint = {
+    "line-opacity": [
+      "case",
+      ["boolean", ["feature-state", "hidden"], false],
+      0,
+      asNumberExpression({
+        symbolization,
+        part: "stroke-opacity",
+        defaultValue: 1,
+      }),
+    ],
+    "line-width": ["interpolate", ["linear"], ["zoom"], 12, 0.5, 16, 4],
+    "line-color": handleSelected(
+      asColorExpression({ symbolization, part: "stroke" }),
+      false,
+      LINE_COLORS_SELECTED,
+    ),
+    "line-dasharray": [
+      "case",
+      ["==", ["get", "status"], "closed"],
+      ["literal", [2, 1]],
+      ["literal", [1, 0]],
+    ],
+  };
   return {
     id: layerId,
     type: "line",
