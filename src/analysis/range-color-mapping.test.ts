3e7ac16e756d4b14069343b5ba8a376ac73892c5
Sam Gathercole <sapagat@fastmail.com>
2025-01-21 18:33:50 +0100

    Only use absolute values for link analysis

--- src/analysis/range-color-mapping.test.ts
+++ src/analysis/range-color-mapping.test.ts
@@ -11,9 +11,25 @@ describe("Range color mapping", () => {
 
     expect(mapping.colorFor(5)).toEqual(mapping.colorFor(0));
     expect(mapping.colorFor(-1)).toEqual(mapping.colorFor(0));
+    expect(mapping.colorFor(-10)).toEqual(mapping.colorFor(0));
     expect(mapping.colorFor(10)).toEqual(mapping.colorFor(15));
     expect(mapping.colorFor(15)).not.toEqual(mapping.colorFor(20));
     expect(mapping.colorFor(20)).toEqual(mapping.colorFor(25));
     expect(mapping.colorFor(20)).toEqual(mapping.colorFor(100));
+  });
+
+  it("when specified assigns to absolute values", () => {
+    const mapping = RangeColorMapping.build({
+      steps: [0, 10, 20],
+      paletteName: "epanet-ramp",
+      unit: "mwc",
+      property: "myprop",
+      absoluteValues: true,
+    });
+
+    expect(mapping.colorFor(-1)).toEqual(mapping.colorFor(0));
+    expect(mapping.colorFor(-10)).toEqual(mapping.colorFor(10));
+    expect(mapping.colorFor(-20)).toEqual(mapping.colorFor(20));
+    expect(mapping.colorFor(-21)).toEqual(mapping.colorFor(21));
   });
 });
