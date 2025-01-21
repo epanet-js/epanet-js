87439c9828d4fe798d578a6c31e264821320ae5a
Sam Gathercole <sapagat@fastmail.com>
2025-01-21 11:06:06 +0100

    Remove FLAG_MAPBOX_PIPE_RESULTS

--- src/lib/colorbrewer.ts
+++ src/lib/colorbrewer.ts
@@ -1,5 +1,3 @@
-import { isFeatureOn } from "src/infra/feature-flags";
-
 export interface CBColors {
   name: string;
   colors: {
@@ -3331,6 +3329,7 @@ export const CARTO_COLOR_QUALITATIVE: CBColors[] = [
   },
 ];
 
+//only customized 5
 const epanetColors: CBColors[] = [
   {
     name: "epanet-ramp",
@@ -3338,9 +3337,7 @@ const epanetColors: CBColors[] = [
       "2": ["#004e64", "#cf597e"],
       "3": ["#004e64", "#8bc480", "#f1d572"],
       "4": ["#004e64", "#68b982", "#c3d86b", "#f1d572"],
-      "5": isFeatureOn("FLAG_MAPBOX_PIPE_RESULTS")
-        ? ["#0c4a6e", "#10b981", "#84cc16", "#eab308", "#f97316"]
-        : ["#004e64", "#52b684", "#94cd74", "#e8c565", "#f99a5d"],
+      "5": ["#0c4a6e", "#10b981", "#84cc16", "#eab308", "#f97316"],
       "6": ["#004e64", "#3fb086", "#71c37d", "#c1d76c", "#f3c761", "#f7745d"],
       "7": [
         "#004e64",
