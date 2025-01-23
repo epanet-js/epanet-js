229cbb6f5111b876c4941057bc0a38c23355c762
Sam Gathercole <sapagat@fastmail.com>
2025-01-23 14:53:18 +0100

    Display history controls in toolbar

--- src/components/toolbar/Toolbar.test.tsx
+++ src/components/toolbar/Toolbar.test.tsx
@@ -3,6 +3,10 @@ import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { Toolbar } from "./Toolbar";
 import userEvent from "@testing-library/user-event";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider as JotaiProvider, createStore } from "jotai";
+import { UIDMap } from "src/lib/id_mapper";
+import { PersistenceContext } from "src/lib/persistence/context";
+import { MemPersistence } from "src/lib/persistence/memory";
 
 describe("Toolbar", () => {
   it("displays button to create new project", () => {
@@ -23,14 +27,32 @@ describe("Toolbar", () => {
     userEvent.click(screen.getByRole("button", { name: /Export/ }));
   });
 
+  it("displays option to undo", () => {
+    renderComponent();
+
+    userEvent.click(screen.getByRole("button", { name: /Undo/ }));
+  });
+
+  it("displays option to redo", () => {
+    renderComponent();
+
+    userEvent.click(screen.getByRole("button", { name: /Redo/ }));
+  });
+
   const renderComponent = () => {
+    const idMap = UIDMap.empty();
     const queryClient = new QueryClient();
+    const store = createStore();
     return render(
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <Toolbar />
-        </TooltipProvider>
-      </QueryClientProvider>,
+      <JotaiProvider store={store}>
+        <PersistenceContext.Provider value={new MemPersistence(idMap, store)}>
+          <QueryClientProvider client={queryClient}>
+            <TooltipProvider>
+              <Toolbar />
+            </TooltipProvider>
+          </QueryClientProvider>
+        </PersistenceContext.Provider>
+      </JotaiProvider>,
     );
   };
 });
