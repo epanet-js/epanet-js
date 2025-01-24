355224ea4d6bfbe55098ceaf6f0cf9e75963ea1e
Sam Gathercole <sapagat@fastmail.com>
2025-01-24 13:36:16 +0100

    Keep history intact if importing invalid file

--- src/components/dialogs/OpenInp.tsx
+++ src/components/dialogs/OpenInp.tsx
@@ -1,27 +1,19 @@
-import type { ConvertResult, InpResult } from "src/lib/convert/utils";
+import type { ConvertResult } from "src/lib/convert/utils";
 import { DialogHeader } from "src/components/dialog";
-import { useContext, useEffect, useState } from "react";
-import { Maybe, Nothing } from "purify-ts/Maybe";
-import { extendExtent, getExtent } from "src/lib/geometry";
-import { BBox } from "src/types";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { getExtent } from "src/lib/geometry";
+import { FeatureCollection } from "src/types";
 import { MapContext } from "src/map";
 import { LngLatBoundsLike } from "mapbox-gl";
-import { flattenResult } from "./import_utils";
 import { translate } from "src/infra/i18n";
 import { FileGroup } from "src/lib/group_files";
 import { OpenInpDialogState } from "src/state/dialog_state";
 import { CrossCircledIcon } from "@radix-ui/react-icons";
-import {
-  DEFAULT_IMPORT_OPTIONS,
-  ImportOptions,
-  Progress,
-} from "src/lib/convert";
-import { Form, Formik, FormikHelpers } from "formik";
-import { FileWarning } from "./import/file_warning";
-import { ImportProgressBar } from "./import/import_progress_bar";
-import SimpleDialogActions, { AckDialogAction } from "./simple_dialog_actions";
-import { useImportFile } from "src/hooks/use_import";
+
+import { AckDialogAction } from "./simple_dialog_actions";
 import { Loading } from "../elements";
+import { parseInp } from "src/import/parse-inp";
+import { usePersistence } from "src/lib/persistence/context";
 
 export type OnNext = (arg0: ConvertResult | null) => void;
 
@@ -35,51 +27,43 @@ export function OpenInpDialog({
   const { files } = modal;
   const map = useContext(MapContext);
 
-  const [extent] = useState<Maybe<BBox>>(Nothing);
   const [isLoading] = useState(true);
   const [error, setError] = useState<boolean>(false);
-  const doImport = useImportFile();
+  const rep = usePersistence();
+  const transactImport = rep.useTransactImport();
 
   const fileGroup = files[0] as FileGroup;
 
-  const handleSuccess: OnNext = (result) => {
-    let nextExtent = extent;
-    if (result) {
-      nextExtent = extendExtent(getExtent(flattenResult(result)), extent);
-    }
-    nextExtent.map((importedExtent) => {
-      map?.map.fitBounds(importedExtent as LngLatBoundsLike, {
-        padding: 100,
-      });
-    });
-    return onClose();
-  };
-
-  const importInp = async () => {
+  const importInp = useCallback(async () => {
     try {
-      const options: ImportOptions = {
-        ...DEFAULT_IMPORT_OPTIONS,
-        type: "inp",
-        toast: false,
-      };
-      const res = (await doImport(
-        fileGroup.file,
-        options,
-        (_newProgress) => {},
-      )) as InpResult;
-      if (res.hydraulicModel.assets.size === 0) {
+      const file = fileGroup.file;
+      const arrayBuffer = await file.arrayBuffer();
+      const content = new TextDecoder().decode(arrayBuffer);
+      const { hydraulicModel, modelMetadata } = parseInp(content);
+      if (hydraulicModel.assets.size === 0) {
         setError(true);
         return;
       }
-      return handleSuccess(res);
+      transactImport(hydraulicModel, modelMetadata, file.name);
+      const features: FeatureCollection = {
+        type: "FeatureCollection",
+        features: [...hydraulicModel.assets.values()].map((a) => a.feature),
+      };
+      const nextExtent = getExtent(features);
+      nextExtent.map((importedExtent) => {
+        map?.map.fitBounds(importedExtent as LngLatBoundsLike, {
+          padding: 100,
+        });
+      });
+      return onClose();
     } catch (error) {
       setError(true);
     }
-  };
+  }, [fileGroup.file, map?.map, onClose, transactImport]);
 
   useEffect(() => {
     importInp();
-  }, []);
+  }, [importInp]);
 
   if (error) {
     return (
@@ -104,65 +88,4 @@ export function OpenInpDialog({
   }
 
   return null;
-}
-
-export function ImportFileGroup({
-  onSuccess,
-  onClose,
-  file: fileGroup,
-}: {
-  onSuccess: OnNext;
-  onClose: () => void;
-  file: FileGroup;
-}) {
-  const { file } = fileGroup;
-  const doImport = useImportFile();
-  const [progress, setProgress] = useState<Progress | null>(null);
-
-  return (
-    <Formik
-      onSubmit={async function onSubmit(
-        options: ImportOptions,
-        helpers: FormikHelpers<ImportOptions>,
-      ) {
-        try {
-          // Don't show a toast if we're going to import
-          // another feature.
-          options = { ...options, toast: true };
-          const res = await doImport(file, options, (newProgress) => {
-            setProgress(newProgress);
-          });
-          if (
-            (res as ConvertResult).type &&
-            (res as ConvertResult).type === "inp"
-          ) {
-            return onSuccess(res as InpResult);
-          }
-        } catch (e: any) {
-          helpers.setErrors({
-            type: e.message,
-          });
-        }
-      }}
-      initialValues={{
-        ...DEFAULT_IMPORT_OPTIONS,
-        type: "inp",
-        text: "",
-        toast: true,
-        secondary: false,
-      }}
-    >
-      <Form>
-        <div>
-          <FileWarning file={fileGroup}>
-            <SimpleDialogActions
-              onClose={onClose}
-              action={translate("openProject")}
-            />
-          </FileWarning>
-          <ImportProgressBar progress={progress} />
-        </div>
-      </Form>
-    </Formik>
-  );
 }
