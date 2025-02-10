07818990c5a0f818ce48c11b57ffc60573ac25ac
Sam Gathercole <sapagat@fastmail.com>
2025-02-10 17:24:44 +0100

    Remove FLAG_NEW_FORM

--- src/hooks/use-new-project.tsx
+++ src/hooks/use-new-project.tsx
@@ -1,18 +1,13 @@
 import { useCallback } from "react";
 import { useAtomValue, useSetAtom } from "jotai";
 import { dialogAtom, hasUnsavedChangesAtom } from "src/state/jotai";
-import { isFeatureOn } from "src/infra/feature-flags";
 
 export const useNewProject = () => {
   const setDialogState = useSetAtom(dialogAtom);
   const hasUnsavedChanges = useAtomValue(hasUnsavedChangesAtom);
 
   const createNew = () => {
-    if (isFeatureOn("FLAG_NEW_FORM")) {
-      setDialogState({ type: "createNew" });
-    } else {
-      window.location.reload();
-    }
+    setDialogState({ type: "createNew" });
   };
 
   return useCallback(
