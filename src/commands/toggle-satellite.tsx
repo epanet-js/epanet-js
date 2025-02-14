537e3878b9758f4f1366ea4ae1523820517c46b2
Sam Gathercole <sapagat@fastmail.com>
2025-02-14 19:20:17 +0100

    Extract toggle satellite command

---
+++ src/commands/toggle-satellite.tsx
@@ -0,0 +1,36 @@
+import { useAtomValue } from "jotai";
+import { useCallback } from "react";
+import LAYERS from "src/lib/default_layers";
+import { newFeatureId } from "src/lib/id";
+import { useLayerConfigState } from "src/map/layer-config";
+import { layerConfigAtom } from "src/state/jotai";
+
+export const useToggleSatellite = () => {
+  const layerConfigs = useAtomValue(layerConfigAtom);
+  const { applyChanges } = useLayerConfigState();
+
+  const toggleSatellite = useCallback(() => {
+    const currentBaseMap = [...layerConfigs.values()][0];
+    const newBaseMap =
+      currentBaseMap.name === LAYERS.MONOCHROME.name
+        ? LAYERS.SATELLITE
+        : LAYERS.MONOCHROME;
+    applyChanges({
+      deleteLayerConfigs: [currentBaseMap.id],
+      putLayerConfigs: [
+        {
+          ...newBaseMap,
+          visibility: true,
+          tms: false,
+          opacity: newBaseMap.opacity,
+          at: currentBaseMap.at,
+          id: newFeatureId(),
+          labelVisibility: true,
+          poiVisibility: true,
+        },
+      ],
+    });
+  }, [layerConfigs, applyChanges]);
+
+  return toggleSatellite;
+};
--- src/map/SatelliteToggle.tsx
+++ src/map/SatelliteToggle.tsx
@@ -1,37 +1,15 @@
 import { useAtomValue } from "jotai";
-import { layerConfigAtom } from "src/state/jotai";
-import { useLayerConfigState } from "./layer-config";
-import LAYERS from "src/lib/default_layers";
-import { newFeatureId } from "src/lib/id";
+import { useToggleSatellite } from "src/commands/toggle-satellite";
 import { mapboxStaticURL } from "src/lib/mapbox_static_url";
+import { layerConfigAtom } from "src/state/jotai";
 
 export const SatelliteToggle = () => {
+  const toggleSatellite = useToggleSatellite();
   const layerConfigs = useAtomValue(layerConfigAtom);
-  const { applyChanges } = useLayerConfigState();
+  const currentBaseMap = [...layerConfigs.values()][0];
+
   if (layerConfigs.size !== 1) return null;
 
-  const currentBaseMap = [...layerConfigs.values()][0];
-  const handleToggle = () => {
-    const newBaseMap =
-      currentBaseMap.name === LAYERS.MONOCHROME.name
-        ? LAYERS.SATELLITE
-        : LAYERS.MONOCHROME;
-    applyChanges({
-      deleteLayerConfigs: [currentBaseMap.id],
-      putLayerConfigs: [
-        {
-          ...newBaseMap,
-          visibility: true,
-          tms: false,
-          opacity: newBaseMap.opacity,
-          at: currentBaseMap.at,
-          id: newFeatureId(),
-          labelVisibility: true,
-          poiVisibility: true,
-        },
-      ],
-    });
-  };
   return (
     <div
       className="absolute bottom-[48px] left-3 w-[92px] h-[92px] mb-2 bg-white rounded border border-white border-2 shadow-md cursor-pointer"
@@ -39,7 +17,7 @@ export const SatelliteToggle = () => {
         backgroundSize: "cover",
         backgroundImage: `url(${mapboxStaticURL(currentBaseMap)})`,
       }}
-      onClick={handleToggle}
+      onClick={() => toggleSatellite()}
     ></div>
   );
 };
