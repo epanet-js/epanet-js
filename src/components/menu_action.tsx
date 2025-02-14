3cb6386ac59fbf7e2094b07105efaaa552a03c02
Sam Gathercole <sapagat@fastmail.com>
2025-02-14 18:59:37 +0100

    Remove extra margin from simulate button

--- src/components/menu_action.tsx
+++ src/components/menu_action.tsx
@@ -47,7 +47,7 @@ export default function MenuAction({
     <div className="relative">
       <Tooltip.Root open={open} onOpenChange={setOpen} delayDuration={200}>
         <div
-          className={`h-10 ${expanded ? "mr-1" : "w-8"} ${
+          className={`h-10 ${expanded ? "" : "w-8"} ${
             disabled ? "opacity-60" : ""
           } group bn flex items-stretch py-1 focus:outline-none`}
         >
