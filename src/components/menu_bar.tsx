74e31c925bba190ad08a8d7824479dcefb22524b
Sam Gathercole <sapagat@fastmail.com>
2025-02-17 10:52:07 +0100

    Extract brand logo component

--- src/components/menu_bar.tsx
+++ src/components/menu_bar.tsx
@@ -40,19 +40,28 @@ function WrappedFeatureCollectionInfo() {
   );
 }
 
+export const BrandLogo = ({ textSize = "md", iconSize = "8", gapX = "0" }) => {
+  return (
+    <span
+      className={`
+          text-gray-500
+          text-${textSize}
+          inline-flex gap-x-${gapX} items-center`}
+      title="Home"
+    >
+      <SiteIcon className={`w-${iconSize} h-${iconSize}`} />
+      epanet-js
+    </span>
+  );
+};
+
 export const MenuBarPlay = memo(function MenuBar() {
   return (
     <div className="flex justify-between h-12 pr-2 text-black dark:text-white">
       <div className="flex items-center">
-        <span
-          className="py-1 pl-2 pr-2
-          text-gray-500
-          inline-flex gap-x-2 items-center"
-          title="Home"
-        >
-          <SiteIcon className="w-8 h-8" />
-          epanet-js
-        </span>
+        <div className="py-1 pl-2 pr-2 inline-flex">
+          <BrandLogo />
+        </div>
         <FileInfo />
       </div>
       <div className="flex items-center gap-x-2">
