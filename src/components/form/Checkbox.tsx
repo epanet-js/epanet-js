f5e0cace6cdd67de89ad47691df9ab8882518307
Sam Gathercole <sapagat@fastmail.com>
2025-02-18 09:36:38 +0100

    Use smaller checkbox

--- src/components/form/Checkbox.tsx
+++ src/components/form/Checkbox.tsx
@@ -1,10 +1,13 @@
 export const Checkbox = ({
+  size = 4,
   ...props
-}: React.InputHTMLAttributes<HTMLInputElement>) => {
+}: {
+  size: number & React.InputHTMLAttributes<HTMLInputElement>;
+}) => {
   return (
     <input
       type="checkbox"
-      className="cursor-pointer w-5 h-5 text-purple-500 bg-gray-200 border-gray-300 rounded focus:ring-purple-500"
+      className={`cursor-pointer w-${size} h-${size} text-purple-500 bg-gray-200 border-gray-300 rounded focus:ring-purple-500`}
       {...props}
     />
   );
