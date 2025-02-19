9307b9904775a6b94dd045594f0e293020e1950b
Sam Gathercole <sapagat@fastmail.com>
2025-02-19 08:36:52 +0100

    Localize welcome page

--- src/global-config.ts
+++ src/global-config.ts
@@ -1,3 +1,4 @@
 export const helpCenterUrl = "https://help.epanetjs.com";
 export const quickStartTutorialUrl =
   "https://www.youtube.com/watch?v=7u4EPdP7WgA";
+export const sourceCodeUrl = "https://github.com";
