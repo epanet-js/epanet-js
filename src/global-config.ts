2bfb12f7561e350ea7085a477b9179d3b29fc066
Sam Gathercole <sapagat@fastmail.com>
2025-02-25 09:47:06 +0100

    Update quick start tutorial video link

--- src/global-config.ts
+++ src/global-config.ts
@@ -1,6 +1,6 @@
 export const helpCenterUrl = "https://help.epanetjs.com";
 export const quickStartTutorialUrl =
-  "https://www.youtube.com/watch?v=7u4EPdP7WgA";
+  "https://www.youtube.com/watch?v=c1FPUm8osuc";
 export const sourceCodeUrl = "https://github.com";
 export const newsletterUrl =
   "https://mastering-water-models.kit.com/e9c8f66e11";
