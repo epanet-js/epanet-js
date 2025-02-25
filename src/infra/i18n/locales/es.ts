87707bb91ead406312587c86d4289fa6d93cdbb6
Sam Gathercole <sapagat@fastmail.com>
2025-02-25 08:41:17 +0100

    Use bold and lighter text

--- src/infra/i18n/locales/es.ts
+++ src/infra/i18n/locales/es.ts
@@ -171,7 +171,7 @@ export const translations: Translations = {
   issuesSummary: "Resumen de las causas",
   useOfUnsupported: "Uso de secciones no soportadas",
   nonDefaultEpanetValues: "Se han detectado valores no por defecto de ${1}",
-  customValueNotSupport: "Valor ${1} no soportado (usando ${2})",
+  customValueNotSupport: "Valor de ${1} no soportado (usando ${2})",
   ignoredValuesDetected: "Se han detectado valores ignorados de ${1}",
   valueIgnored: "Valor de ${1} es ignorado (usando ${2})",
   nodesMissingCoordinates: "Nudos sin coordenadas",
