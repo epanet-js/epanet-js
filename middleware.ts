2e3eb7f3650598832ee5f6a4c1d591930cd2df65
lbutler <lukepbutler@gmail.com>
2025-03-09 21:37:32 -0400

    Verify domain for azure

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,8 @@ export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.endsWith("js.map")) return NextResponse.next();
   if (request.nextUrl.pathname.startsWith("/api/auth-webhook"))
     return NextResponse.next();
+  if (request.nextUrl.pathname.startsWith("/.well-known/"))
+    return NextResponse.next();
 
   const authHeader = request.headers.get("Authorization");
   if (!authHeader) {
@@ -19,7 +21,7 @@ export function middleware(request: NextRequest) {
   }
   const base64Credentials = authHeader.split(" ")[1];
   const credentials = Buffer.from(base64Credentials, "base64").toString(
-    "utf-8",
+    "utf-8"
   );
   const [username, password] = credentials.split(":");
 
