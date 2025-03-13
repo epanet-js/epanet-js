0d7e1b89cdf8ac561305dd36d40e3df2c707b614
Sam Gathercole <sapagat@fastmail.com>
2025-03-13 13:31:53 +0100

    Use delete selected assets command

--- src/hooks/use_map_keybindings.ts
+++ src/hooks/use_map_keybindings.ts
@@ -1,12 +1,9 @@
 import type { Options } from "react-hotkeys-hook";
 import { useHotkeys } from "src/keyboard/hotkeys";
 import { dataAtom, selectionAtom } from "src/state/jotai";
-import { usePersistence } from "src/lib/persistence/context";
 import { filterLockedFeatures } from "src/lib/folder";
-import { USelection } from "src/selection";
 import { useCallback } from "react";
 import { useAtomCallback } from "jotai/utils";
-import { deleteAssets } from "src/hydraulic-model/model-operations";
 import { useUserTracking } from "src/infra/user-tracking";
 
 const IGNORE_ROLES = new Set(["menuitem"]);
@@ -22,8 +19,6 @@ export const keybindingOptions: Options = {
 };
 
 export function useMapKeybindings() {
-  const rep = usePersistence();
-  const transact = rep.useTransact();
   const userTracking = useUserTracking();
 
   const onSelectAll = useAtomCallback(
@@ -51,42 +46,5 @@ export function useMapKeybindings() {
     },
     [onSelectAll],
     "SELECT_ALL",
-  );
-
-  const onDelete = useAtomCallback(
-    useCallback(
-      (get, set) => {
-        const data = get(dataAtom);
-        set(selectionAtom, USelection.none());
-        const { hydraulicModel, selection } = data;
-
-        const assetIds = USelection.toIds(selection);
-        const moment = deleteAssets(hydraulicModel, {
-          assetIds,
-        });
-        userTracking.capture({
-          name: "assets.deleted",
-          source: "shortcut",
-          count: assetIds.length,
-        });
-
-        transact(moment);
-        return false;
-      },
-      [transact, userTracking],
-    ),
-  );
-
-  useHotkeys(
-    ["backspace", "del"],
-    (e) => {
-      if (IGNORE_ROLES.has((e.target as HTMLElement).getAttribute("role")!))
-        return;
-
-      e.preventDefault();
-      void onDelete();
-    },
-    [onDelete],
-    "DELETE",
   );
 }
