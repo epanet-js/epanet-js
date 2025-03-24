a1ccde66df669ecc17251c2ae520ec24ad11626a
Sam Gathercole <sapagat@fastmail.com>
2025-03-24 17:37:21 +0100

    Preserve labels state when switching mapbox layers

--- src/commands/toggle-satellite.tsx
+++ src/commands/toggle-satellite.tsx
@@ -6,6 +6,8 @@ import { useLayerConfigState } from "src/map/layer-config";
 import { layerConfigAtom } from "src/state/jotai";
 import { useAuth } from "src/auth";
 import { ILayerConfig } from "src/types";
+import { isFeatureOn } from "src/infra/feature-flags";
+import { maybeDeleteOldMapboxLayer } from "src/components/layers/popover";
 
 export const satelliteLimitedZoom = 16;
 
@@ -17,6 +19,36 @@ export const useToggleSatellite = () => {
   const { isSignedIn } = useAuth();
 
   const toggleSatellite = useCallback(() => {
+    const items = [...layerConfigs.values()];
+
+    const { deleteLayerConfigs, oldAt, oldMapboxLayer } =
+      maybeDeleteOldMapboxLayer(items);
+
+    const newBaseMap =
+      oldMapboxLayer && oldMapboxLayer.name === LAYERS.MONOCHROME.name
+        ? LAYERS.SATELLITE
+        : LAYERS.MONOCHROME;
+
+    const newLayerConfig: ILayerConfig = {
+      ...newBaseMap,
+      visibility: true,
+      tms: false,
+      opacity: newBaseMap.opacity,
+      at: oldAt || "a0",
+      id: newFeatureId(),
+      labelVisibility: oldMapboxLayer ? oldMapboxLayer.labelVisibility : true,
+    };
+
+    if (!isSignedIn && newBaseMap.name === LAYERS.SATELLITE.name) {
+      newLayerConfig.sourceMaxZoom["mapbox-satellite"] = satelliteLimitedZoom;
+    }
+    applyChanges({
+      deleteLayerConfigs,
+      putLayerConfigs: [newLayerConfig],
+    });
+  }, [layerConfigs, applyChanges, isSignedIn]);
+
+  const toggleSatelliteDeprecated = useCallback(() => {
     const currentBaseMap = [...layerConfigs.values()][0];
     const newBaseMap =
       currentBaseMap.name === LAYERS.MONOCHROME.name
@@ -44,5 +76,7 @@ export const useToggleSatellite = () => {
     });
   }, [layerConfigs, applyChanges, isSignedIn]);
 
-  return toggleSatellite;
+  return isFeatureOn("FLAG_LAYERS")
+    ? toggleSatellite
+    : toggleSatelliteDeprecated;
 };
