0b4c05be29a602abebc658d586bb5919a9dfc864
lbutler <lukepbutler@gmail.com>
2025-03-26 22:03:30 -0400

    Update quick start tutorial url

--- src/global-config.ts
+++ src/global-config.ts
@@ -1,6 +1,6 @@
 export const helpCenterUrl = "https://help.epanetjs.com";
 export const quickStartTutorialUrl =
-  "https://www.notion.so/Getting-started-1a5e18c9f0f680399659f710a37aa452?pvs=4";
+  "https://help.epanetjs.com/Getting-started-1a5e18c9f0f680399659f710a37aa452";
 export const sourceCodeUrl = "https://github.com";
 export const newsletterUrl =
   "https://mastering-water-models.kit.com/e9c8f66e11";
