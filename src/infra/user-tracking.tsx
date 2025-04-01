40cbf77e1b5ffbf1e29cb62af3096c8b0977f18d
Sam Gathercole <sapagat@fastmail.com>
2025-04-01 17:13:42 +0200

    Capture event when upgrade button clicked from menu

--- src/infra/user-tracking.tsx
+++ src/infra/user-tracking.tsx
@@ -359,6 +359,7 @@ type UserEvent =
   | { name: "addCustomLayer.clicked" }
   | { name: "layerType.choosen"; type: string }
   | { name: "checkout.started"; plan: string; paymentType: string }
+  | { name: "upgradeButton.clicked"; source: "menu" }
   | {
       name: "baseMap.changed";
       oldValue: string;
