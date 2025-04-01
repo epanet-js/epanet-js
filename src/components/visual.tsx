1ba05276ef809d2d8423d5fc797bf1901321f1bf
Sam Gathercole <sapagat@fastmail.com>
2025-04-01 13:49:33 +0200

    Open upgrade dialog when clicking on payed feature

--- src/components/visual.tsx
+++ src/components/visual.tsx
@@ -1,6 +1,5 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { LayersIcon } from "@radix-ui/react-icons";
-import * as T from "@radix-ui/react-tooltip";
 import * as E from "src/components/elements";
 import { Root, Trigger } from "@radix-ui/react-popover";
 import { LayersPopover } from "./layers/popover";
@@ -9,39 +8,38 @@ import { useUserTracking } from "src/infra/user-tracking";
 
 export const Visual = () => {
   const userTracking = useUserTracking();
+  const [isOpen, setOpen] = useState<boolean>(false);
 
   return (
     <div className="flex items-center">
-      <T.Root>
-        <Root>
-          <div className="p-2 flex items-stretch">
-            <T.Trigger asChild>
-              <Trigger aria-label="Layers" asChild>
-                <E.Button
-                  variant="quiet"
-                  onClick={() => {
-                    userTracking.capture({
-                      name: "layersPopover.opened",
-                      source: "toolbar",
-                    });
-                  }}
-                >
-                  <LayersIcon />
-                  {translate("layers")}
-                </E.Button>
-              </Trigger>
-            </T.Trigger>
-          </div>
-          <E.PopoverContent2 size="md">
-            <Suspense fallback={<E.Loading size="sm" />}>
-              <LayersPopover />
-            </Suspense>
-          </E.PopoverContent2>
-        </Root>
-        <E.TContent side="bottom">
-          <span className="whitespace-nowrap">Manage background layers</span>
-        </E.TContent>
-      </T.Root>
+      <Root
+        open={isOpen}
+        onOpenChange={(val) => {
+          setOpen(val);
+        }}
+      >
+        <div className="p-2 flex items-stretch">
+          <Trigger aria-label="Layers" asChild>
+            <E.Button
+              variant="quiet"
+              onClick={() => {
+                userTracking.capture({
+                  name: "layersPopover.opened",
+                  source: "toolbar",
+                });
+              }}
+            >
+              <LayersIcon />
+              {translate("layers")}
+            </E.Button>
+          </Trigger>
+        </div>
+        <E.PopoverContent2 size="md">
+          <Suspense fallback={<E.Loading size="sm" />}>
+            <LayersPopover onClose={() => setOpen(false)} />
+          </Suspense>
+        </E.PopoverContent2>
+      </Root>
     </div>
   );
 };
