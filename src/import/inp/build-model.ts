778d25fc59265421cef2b5f5f5af860af446ab29
Sam Gathercole <sapagat@fastmail.com>
2025-04-15 10:52:16 +0200

    Refactor parser methods to append data to model

--- src/import/inp/build-model.ts
+++ src/import/inp/build-model.ts
@@ -1,5 +1,13 @@
 import { HydraulicModel, initializeHydraulicModel } from "src/hydraulic-model";
-import { InpData, ItemData } from "./inp-data";
+import {
+  InpData,
+  ItemData,
+  JunctionData,
+  PipeData,
+  PumpData,
+  ReservoirData,
+  TankData,
+} from "./inp-data";
 import { IssuesAccumulator } from "./issues";
 import { ModelMetadata } from "src/model-metadata";
 import { Quantities, presets } from "src/model-metadata/quantities-spec";
@@ -20,162 +28,258 @@ export const buildModel = (
   });
 
   for (const junctionData of inpData.junctions) {
-    const coordinates = getNodeCoordinates(inpData, junctionData.id, issues);
-    if (!coordinates) continue;
-
-    const demand = calculateJunctionDemand(
-      junctionData,
-      inpData.demands,
-      inpData.patterns,
-    );
-
-    const junction = hydraulicModel.assetBuilder.buildJunction({
-      label: junctionData.id,
-      coordinates,
-      elevation: junctionData.elevation,
-      demand,
-    });
-    hydraulicModel.assets.set(junction.id, junction);
-    nodeIds.set(junctionData.id, junction.id);
+    addJunction(hydraulicModel, junctionData, { inpData, issues, nodeIds });
   }
 
   for (const reservoirData of inpData.reservoirs) {
-    const coordinates = getNodeCoordinates(inpData, reservoirData.id, issues);
-    if (!coordinates) continue;
-
-    const reservoir = hydraulicModel.assetBuilder.buildReservoir({
-      label: reservoirData.id,
-      coordinates,
-      head: calculateReservoirHead(reservoirData, inpData.patterns),
+    addReservoir(hydraulicModel, reservoirData, {
+      inpData,
+      issues,
+      nodeIds,
     });
-    hydraulicModel.assets.set(reservoir.id, reservoir);
-    nodeIds.set(reservoirData.id, reservoir.id);
   }
 
   for (const tankData of inpData.tanks) {
-    const coordinates = getNodeCoordinates(inpData, tankData.id, issues);
-    if (!coordinates) continue;
-
-    const reservoir = hydraulicModel.assetBuilder.buildReservoir({
-      label: tankData.id,
-      coordinates,
-      head: tankData.elevation + tankData.initialLevel,
+    addTankAsReservoir(hydraulicModel, tankData, {
+      inpData,
+      issues,
+      nodeIds,
     });
-    hydraulicModel.assets.set(reservoir.id, reservoir);
-    nodeIds.set(tankData.id, reservoir.id);
   }
 
   for (const pumpData of inpData.pumps) {
-    const startCoordinates = getNodeCoordinates(
+    addPump(hydraulicModel, pumpData, {
       inpData,
-      pumpData.startNodeDirtyId,
       issues,
-    );
-    const endCoordinates = getNodeCoordinates(
-      inpData,
-      pumpData.endNodeDirtyId,
-      issues,
-    );
-    const vertices = getVertices(inpData, pumpData.id, issues);
-    if (!startCoordinates || !endCoordinates) continue;
-
-    const startNodeId = nodeIds.get(pumpData.startNodeDirtyId);
-    const endNodeId = nodeIds.get(pumpData.endNodeDirtyId);
-
-    if (!startNodeId || !endNodeId) continue;
-
-    let definitionProps = {};
-
-    if (pumpData.power !== undefined) {
-      definitionProps = {
-        definitionType: "power",
-        power: pumpData.power,
-      };
-    }
-
-    if (pumpData.curveId !== undefined) {
-      const curvePoints = inpData.curves.get(pumpData.curveId) || [];
-
-      const middleIndex = Math.floor(curvePoints.length / 2);
-      const point = curvePoints[middleIndex];
-
-      if (point) {
-        definitionProps = {
-          definitionType: "flow-vs-head",
-          designFlow: point.x,
-          designHead: point.y,
-        };
-      }
-    }
-
-    let initialStatus: PumpStatus = "on";
-    let speed = pumpData.speed !== undefined ? pumpData.speed : 1;
-
-    if (inpData.status.has(pumpData.id)) {
-      const statusValue = inpData.status.get(pumpData.id) as string;
-      if (statusValue === "CLOSED") {
-        initialStatus = "off";
-      } else if (statusValue === "OPEN") {
-        initialStatus = "on";
-        speed = 1;
-      } else if (!isNaN(parseFloat(statusValue))) {
-        speed = parseFloat(statusValue);
-      }
-    }
-
-    const pump = hydraulicModel.assetBuilder.buildPump({
-      label: pumpData.id,
-      connections: [startNodeId, endNodeId],
-      ...definitionProps,
-      initialStatus,
-      speed,
-      coordinates: [startCoordinates, ...vertices, endCoordinates],
+      nodeIds,
     });
-    hydraulicModel.assets.set(pump.id, pump);
-    hydraulicModel.topology.addLink(pump.id, startNodeId, endNodeId);
   }
 
   for (const pipeData of inpData.pipes) {
-    const startCoordinates = getNodeCoordinates(
-      inpData,
-      pipeData.startNodeDirtyId,
-      issues,
-    );
-    const endCoordinates = getNodeCoordinates(
-      inpData,
-      pipeData.endNodeDirtyId,
-      issues,
-    );
-    const vertices = getVertices(inpData, pipeData.id, issues);
-    if (!startCoordinates || !endCoordinates) continue;
-
-    const startNodeId = nodeIds.get(pipeData.startNodeDirtyId);
-    const endNodeId = nodeIds.get(pipeData.endNodeDirtyId);
-
-    if (!startNodeId || !endNodeId) continue;
-
-    let status = pipeData.status;
-
-    if (inpData.status.has(pipeData.id)) {
-      const statusValue = inpData.status.get(pipeData.id) as string;
-      status = statusValue === "CLOSED" ? "closed" : "open";
-    }
-
-    const pipe = hydraulicModel.assetBuilder.buildPipe({
-      label: pipeData.id,
-      length: pipeData.length,
-      diameter: pipeData.diameter,
-      minorLoss: pipeData.minorLoss,
-      roughness: pipeData.roughness,
-      connections: [startNodeId, endNodeId],
-      status,
-      coordinates: [startCoordinates, ...vertices, endCoordinates],
-    });
-    hydraulicModel.assets.set(pipe.id, pipe);
-    hydraulicModel.topology.addLink(pipe.id, startNodeId, endNodeId);
+    addPipe(hydraulicModel, pipeData, { inpData, issues, nodeIds });
   }
 
   return { hydraulicModel, modelMetadata: { quantities } };
+};
+
+const addJunction = (
+  hydraulicModel: HydraulicModel,
+  junctionData: JunctionData,
+  {
+    inpData,
+    issues,
+    nodeIds,
+  }: {
+    inpData: InpData;
+    issues: IssuesAccumulator;
+    nodeIds: ItemData<string>;
+  },
+) => {
+  const coordinates = getNodeCoordinates(inpData, junctionData.id, issues);
+  if (!coordinates) return;
+
+  const demand = calculateJunctionDemand(
+    junctionData,
+    inpData.demands,
+    inpData.patterns,
+  );
+
+  const junction = hydraulicModel.assetBuilder.buildJunction({
+    label: junctionData.id,
+    coordinates,
+    elevation: junctionData.elevation,
+    demand,
+  });
+  hydraulicModel.assets.set(junction.id, junction);
+  nodeIds.set(junctionData.id, junction.id);
+};
+
+const addReservoir = (
+  hydraulicModel: HydraulicModel,
+  reservoirData: ReservoirData,
+  {
+    inpData,
+    issues,
+    nodeIds,
+  }: {
+    inpData: InpData;
+    issues: IssuesAccumulator;
+    nodeIds: ItemData<string>;
+  },
+) => {
+  const coordinates = getNodeCoordinates(inpData, reservoirData.id, issues);
+  if (!coordinates) return;
+
+  const reservoir = hydraulicModel.assetBuilder.buildReservoir({
+    label: reservoirData.id,
+    coordinates,
+    head: calculateReservoirHead(reservoirData, inpData.patterns),
+  });
+  hydraulicModel.assets.set(reservoir.id, reservoir);
+  nodeIds.set(reservoirData.id, reservoir.id);
+};
+
+const addTankAsReservoir = (
+  hydraulicModel: HydraulicModel,
+  tankData: TankData,
+  {
+    inpData,
+    issues,
+    nodeIds,
+  }: {
+    inpData: InpData;
+    issues: IssuesAccumulator;
+    nodeIds: ItemData<string>;
+  },
+) => {
+  const coordinates = getNodeCoordinates(inpData, tankData.id, issues);
+  if (!coordinates) return;
+
+  const reservoir = hydraulicModel.assetBuilder.buildReservoir({
+    label: tankData.id,
+    coordinates,
+    head: tankData.elevation + tankData.initialLevel,
+  });
+  hydraulicModel.assets.set(reservoir.id, reservoir);
+  nodeIds.set(tankData.id, reservoir.id);
+};
+
+const addPump = (
+  hydraulicModel: HydraulicModel,
+  pumpData: PumpData,
+  {
+    inpData,
+    issues,
+    nodeIds,
+  }: {
+    inpData: InpData;
+    issues: IssuesAccumulator;
+    nodeIds: ItemData<string>;
+  },
+) => {
+  const linkProperties = getLinkProperties(inpData, issues, nodeIds, pumpData);
+  if (!linkProperties) return;
+
+  const { coordinates, connections } = linkProperties;
+
+  let definitionProps = {};
+
+  if (pumpData.power !== undefined) {
+    definitionProps = {
+      definitionType: "power",
+      power: pumpData.power,
+    };
+  }
+
+  if (pumpData.curveId !== undefined) {
+    const curvePoints = inpData.curves.get(pumpData.curveId) || [];
+
+    const middleIndex = Math.floor(curvePoints.length / 2);
+    const point = curvePoints[middleIndex];
+
+    if (point) {
+      definitionProps = {
+        definitionType: "flow-vs-head",
+        designFlow: point.x,
+        designHead: point.y,
+      };
+    }
+  }
+
+  let initialStatus: PumpStatus = "on";
+  let speed = pumpData.speed !== undefined ? pumpData.speed : 1;
+
+  if (inpData.status.has(pumpData.id)) {
+    const statusValue = inpData.status.get(pumpData.id) as string;
+    if (statusValue === "CLOSED") {
+      initialStatus = "off";
+    } else if (statusValue === "OPEN") {
+      initialStatus = "on";
+      speed = 1;
+    } else if (!isNaN(parseFloat(statusValue))) {
+      speed = parseFloat(statusValue);
+    }
+  }
+
+  const pump = hydraulicModel.assetBuilder.buildPump({
+    label: pumpData.id,
+    connections,
+    ...definitionProps,
+    initialStatus,
+    speed,
+    coordinates,
+  });
+  hydraulicModel.assets.set(pump.id, pump);
+  hydraulicModel.topology.addLink(pump.id, connections[0], connections[1]);
+};
+
+const addPipe = (
+  hydraulicModel: HydraulicModel,
+  pipeData: PipeData,
+  {
+    inpData,
+    issues,
+    nodeIds,
+  }: {
+    inpData: InpData;
+    issues: IssuesAccumulator;
+    nodeIds: ItemData<string>;
+  },
+) => {
+  const linkProperties = getLinkProperties(inpData, issues, nodeIds, pipeData);
+  if (!linkProperties) return;
+  const { connections, coordinates } = linkProperties;
+
+  let status = pipeData.status;
+
+  if (inpData.status.has(pipeData.id)) {
+    const statusValue = inpData.status.get(pipeData.id) as string;
+    status = statusValue === "CLOSED" ? "closed" : "open";
+  }
+
+  const pipe = hydraulicModel.assetBuilder.buildPipe({
+    label: pipeData.id,
+    length: pipeData.length,
+    diameter: pipeData.diameter,
+    minorLoss: pipeData.minorLoss,
+    roughness: pipeData.roughness,
+    status,
+    connections,
+    coordinates,
+  });
+  hydraulicModel.assets.set(pipe.id, pipe);
+  hydraulicModel.topology.addLink(pipe.id, connections[0], connections[1]);
+};
+
+const getLinkProperties = (
+  inpData: InpData,
+  issues: IssuesAccumulator,
+  nodeIds: ItemData<string>,
+  linkData: { id: string; startNodeDirtyId: string; endNodeDirtyId: string },
+) => {
+  const startCoordinates = getNodeCoordinates(
+    inpData,
+    linkData.startNodeDirtyId,
+    issues,
+  );
+  const endCoordinates = getNodeCoordinates(
+    inpData,
+    linkData.endNodeDirtyId,
+    issues,
+  );
+  const vertices = getVertices(inpData, linkData.id, issues);
+
+  if (!startCoordinates || !endCoordinates) return null;
+
+  const startNodeId = nodeIds.get(linkData.startNodeDirtyId);
+  const endNodeId = nodeIds.get(linkData.endNodeDirtyId);
+
+  if (!startNodeId || !endNodeId) return null;
+
+  return {
+    coordinates: [startCoordinates, ...vertices, endCoordinates],
+    connections: [startNodeId, endNodeId] as [string, string],
+  };
 };
 
 const getVertices = (
@@ -208,6 +312,7 @@ const getNodeCoordinates = (
   }
   return nodeCoordinates;
 };
+
 const isWgs84 = (coordinates: Position) =>
   coordinates[0] >= -180 &&
   coordinates[0] <= 180 &&
