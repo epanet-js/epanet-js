c18b35747d2e480e59a6d0becaf875102b736218
Sam Gathercole <sapagat@fastmail.com>
2025-04-25 11:17:34 +0200

    Ensure valve icons are same size

--- src/map/icons/dynamic-icons.ts
+++ src/map/icons/dynamic-icons.ts
@@ -55,20 +55,28 @@ export const buildPrvSvg = ({
   fillColor?: string;
 } = {}) => {
   return `
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="${width}"
-  height="${height}"
-  viewBox="0 0 780 780"
-  version="1.1"
->
-  <g transform="rotate(90 376.55 390)" stroke-linejoin="round">
-    <rect x="31.2" y="31.2" width="717.6" height="717.6" ry="171.86" fill="${fillColor}" opacity=".99" stroke="${borderColor}" stroke-linecap="round" stroke-width="35.498"/>
-    <path d="m342.33 391.63 261.9-231.74v462.09z" fill="${triangleColor}" stroke="${triangleColor}" stroke-width="35.498"/>
-    <path d="m341.83 391.5-166.73 151.89v-302.86z" fill="${fillColor}" stroke="${triangleColor}" stroke-width="35.494"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 815.5 815.5" version="1.1">
+  <g transform="rotate(-90,407.75,390)" style="stroke-width:62.4">
+    <rect
+      x="31.2"
+      y="31.2"
+      width="717.6"
+      height="717.6"
+      ry="171.86"
+      style="fill:${fillColor};stroke:${borderColor};stroke-width:35.5;stroke-linecap:round;stroke-linejoin:round"
+    />
+    <path
+      d="M436.8,390L600.6,239.57v299.95z"
+      style="fill:${fillColor};stroke:${triangleColor};stroke-width:44.95;stroke-linecap:butt;stroke-linejoin:round"
+    />
+    <path
+      d="M447.98,390L179.6,631.54V149.91z"
+      style="fill:${triangleColor};stroke:${triangleColor};stroke-width:47.11;stroke-linecap:butt;stroke-linejoin:round"
+    />
   </g>
 </svg>
-`;
+
+  `;
 };
 
 export const buildPsvSvg = ({
@@ -85,20 +93,27 @@ export const buildPsvSvg = ({
   fillColor?: string;
 } = {}) => {
   return `
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="${width}"
-  height="${height}"
-  viewBox="0 0 780 780"
-  version="1.1"
->
-  <g transform="rotate(270 376.55 390)" stroke-linejoin="round">
-    <rect x="31.2" y="31.2" width="717.6" height="717.6" ry="171.86" fill="${fillColor}" opacity=".99" stroke="${borderColor}" stroke-linecap="round" stroke-width="35.498"/>
-    <path d="m342.33 391.63 261.9-231.74v462.09z" fill="${fillColor}" stroke="${triangleColor}" stroke-width="35.498"/>
-    <path d="m341.83 391.5-166.73 151.89v-302.86z" fill="${triangleColor}" stroke="${triangleColor}" stroke-width="35.494"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 815.5 815.5" version="1.1">
+  <g transform="rotate(-90,407.75,390)" style="stroke-width:62.4">
+    <rect
+      x="31.2"
+      y="31.2"
+      width="717.6"
+      height="717.6"
+      ry="171.86"
+      style="fill:${fillColor};stroke:${borderColor};stroke-width:35.5;stroke-linecap:round;stroke-linejoin:round"
+    />
+    <path
+      d="M343.2,390L179.4,239.57v299.95z"
+      style="fill:${triangleColor};stroke:${triangleColor};stroke-width:44.95;stroke-linecap:butt;stroke-linejoin:round"
+    />
+    <path
+      d="M332.02,390L600.4,631.54V149.91z"
+      style="fill:${fillColor};stroke:${triangleColor};stroke-width:47.11;stroke-linecap:butt;stroke-linejoin:round"
+    />
   </g>
 </svg>
-`;
+  `;
 };
 
 export const buildGpvSvg = ({
@@ -158,33 +173,34 @@ export const buildFcvSvg = ({
   fillColor?: string;
 } = {}) => {
   return `
-  <svg
+<svg
   xmlns="http://www.w3.org/2000/svg"
   width="${width}"
   height="${height}"
   viewBox="0 0 815.5 815.5"
   version="1.1"
 >
-  <g transform="rotate(90,407.75,390)" style="stroke-width:62.4">
+  <g transform="rotate(-90,407.75,390)" style="stroke-width:62.4">
     <rect
-      x="31.2"
+      x="-748.8"
       y="31.2"
       width="717.6"
       height="717.6"
       ry="171.86"
       style="fill:${fillColor};stroke:${borderColor};stroke-width:35.5;stroke-linecap:round;stroke-linejoin:round"
+      transform="scale(-1,1)"
     />
     <path
-      d="M171.31,390 L399.01,209.19v360.53z"
-      style="fill:${triangleColor};stroke:${triangleColor};stroke-width:37.54;stroke-linecap:butt;stroke-linejoin:round"
+      d="M389.62,388.66L187.2,206.83v362.57z"
+      style="fill:${fillColor};stroke:${triangleColor};stroke-width:35.5;stroke-linecap:butt;stroke-linejoin:round"
     />
     <path
-      d="M405.6,390 L608.02,208.17v362.57z"
-      style="fill:none;stroke:${triangleColor};stroke-width:35.5;stroke-linecap:butt;stroke-linejoin:round"
+      d="M595.98,390l-202.42,181.83V209.26z"
+      style="fill:${triangleColor};stroke:${triangleColor};stroke-width:35.5;stroke-linecap:butt;stroke-linejoin:round"
     />
   </g>
 </svg>
-  `;
+`;
 };
 
 export const buildPbvSvg = ({
@@ -201,14 +217,8 @@ export const buildPbvSvg = ({
   fillColor?: string;
 } = {}) => {
   return `
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="${width}"
-  height="${height}j"
-  viewBox="0 0 815.5 815.5"
-  version="1.1"
->
-  <g transform="rotate(90,407.75,390)" style="stroke-width:62.4">
+<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 815.5 815.5" version="1.1">
+  <g transform="rotate(-90,407.75,390)" style="stroke-width:62.4">
     <rect
       x="31.2"
       y="31.2"
@@ -219,15 +229,15 @@ export const buildPbvSvg = ({
     />
     <path
       d="M390.38,388.66L592.8,206.83v362.57z"
-      style="fill:${triangleColor};stroke:${triangleColor};stroke-width:35.5;stroke-linecap:butt;stroke-linejoin:round"
+      style="fill:${fillColor};stroke:${triangleColor};stroke-width:35.5;stroke-linecap:butt;stroke-linejoin:round"
     />
     <path
       d="M386.44,390L184.02,571.83V209.26z"
-      style="fill:${fillColor};stroke:${triangleColor};stroke-width:35.49;stroke-linecap:butt;stroke-linejoin:round"
+      style="fill:${triangleColor};stroke:${triangleColor};stroke-width:35.5;stroke-linecap:butt;stroke-linejoin:round"
     />
     <path
-      d="M390,210.6v358.8"
-      style="fill:none;stroke:${triangleColor};stroke-width:34.05;stroke-linecap:round"
+      d="M390,195v382.2"
+      style="fill:none;stroke:${triangleColor};stroke-width:35.5;stroke-linecap:round;stroke-linejoin:miter"
     />
   </g>
 </svg>
