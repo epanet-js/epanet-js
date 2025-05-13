62b474643d370ae4ca9f76ec01eb0118a19bef61
Sam Gathercole <sapagat@fastmail.com>
2025-05-13 08:52:23 +0200

    Disable shortcuts when dialog open

--- src/keyboard/hotkeys.ts
+++ src/keyboard/hotkeys.ts
@@ -1,7 +1,9 @@
+import { useAtomValue } from "jotai";
 import Mousetrap from "mousetrap";
 import { useEffect } from "react";
 import { isDebugOn } from "src/infra/debug-mode";
 import { getIsMac } from "src/lib/utils";
+import { dialogAtom } from "src/state/dialog_state";
 
 type DependencyList = ReadonlyArray<unknown>;
 
@@ -13,6 +15,8 @@ export const useHotkeys = (
 ) => {
   const keysList = Array.isArray(keys) ? keys : [keys];
   const localizedKeys = keysList.map(translateCommandForMac);
+  const dialog = useAtomValue(dialogAtom);
+
   useEffect(() => {
     if (isDebugOn) {
       // eslint-disable-next-line no-console
@@ -21,12 +25,14 @@ export const useHotkeys = (
       );
     }
 
+    if (!!dialog) return;
+
     Mousetrap.bind(localizedKeys, fn);
     return () => {
       Mousetrap.unbind(localizedKeys);
     };
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencyList);
+  }, [...dependencyList, dialog]);
 };
 
 const translateCommandForMac = (hotkey: string): string => {
