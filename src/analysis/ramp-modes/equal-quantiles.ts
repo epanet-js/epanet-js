87c7a44768f973dbb431db4e9814c19f88d47d85
Sam Gathercole <sapagat@fastmail.com>
2025-05-13 18:41:03 +0200

    Implement function to check if data valid for equal intervals

--- src/analysis/ramp-modes/equal-quantiles.ts
+++ src/analysis/ramp-modes/equal-quantiles.ts
@@ -1,32 +1,5 @@
 import { roundToSignificantDigits } from "src/infra/rounding";
 
-export const calculateEqualQuantileBreaks = (
-  sortedData: number[],
-  numBreaks: number,
-): number[] => {
-  const n = sortedData.length;
-  const breaks = [];
-
-  for (let i = 1; i <= numBreaks; i++) {
-    const quantile = i / (numBreaks + 1);
-    const index = quantile * (n - 1);
-
-    if (Number.isInteger(index)) {
-      breaks.push(sortedData[index]);
-    } else {
-      const lowerIndex = Math.floor(index);
-      const upperIndex = Math.ceil(index);
-      const fractionalPart = index - lowerIndex;
-      const interpolatedValue =
-        sortedData[lowerIndex] +
-        (sortedData[upperIndex] - sortedData[lowerIndex]) * fractionalPart;
-      breaks.push(roundToSignificantDigits(interpolatedValue));
-    }
-  }
-
-  return breaks;
-};
-
 export const calculateEqualQuantilesRange = (
   sortedData: number[],
   numIntervals: number,
