aeb7fee9333ee458122bd4943e62245076685d1e
Sam Gathercole <sapagat@fastmail.com>
2025-06-10 11:29:50 +0200

    Use ping to determine when online again

--- src/components/offline-guard.tsx
+++ src/components/offline-guard.tsx
@@ -1,20 +1,50 @@
 import { Link1Icon, LinkBreak1Icon } from "@radix-ui/react-icons";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { hideNotification, notify } from "./notifications";
-import { atom, useAtom } from "jotai";
-
-export const offlineAtom = atom<boolean | undefined>(undefined);
 
 const offlineToastId = "offline-toast";
 const onlineToastId = "online-toast";
+const pingUrl = "https://www.cloudflare.com/cdn-cgi/trace";
 
 export const useOfflineStatus = () => {
-  const [isOffline, setOfflineAtom] = useAtom(offlineAtom);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const isOfflineRef = useRef<boolean>(false);
+
+  const cancelConnectivityCheck = useCallback(() => {
+    if (!intervalRef.current) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  }, []);
+
+  const startConnectivityCheck = useCallback(() => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(async () => {
+      try {
+        const response = await fetch(pingUrl, {
+          method: "HEAD",
+        });
+        if (response.ok) {
+          isOfflineRef.current = false;
+          hideNotification(offlineToastId);
+          notify({
+            variant: "success",
+            title: "Connection restored!",
+            Icon: Link1Icon,
+            duration: 3000,
+            id: onlineToastId,
+          });
+          cancelConnectivityCheck();
+          return;
+        }
+      } catch (e) {}
+    }, 5000);
+  }, [cancelConnectivityCheck]);
 
   const setOffline = useCallback(() => {
-    if (isOffline) return;
+    if (isOfflineRef.current) return;
 
-    setOfflineAtom(true);
+    isOfflineRef.current = true;
     hideNotification(onlineToastId);
     notify({
       variant: "warning",
@@ -25,44 +55,26 @@ export const useOfflineStatus = () => {
       dismissable: false,
       id: offlineToastId,
     });
-  }, [isOffline, setOfflineAtom]);
+    startConnectivityCheck();
+  }, [startConnectivityCheck]);
 
-  const setOnline = useCallback(() => {
-    if (!isOffline) return;
-
-    setOfflineAtom(false);
-    hideNotification(offlineToastId);
-    notify({
-      variant: "success",
-      title: "Connection restored!",
-      Icon: Link1Icon,
-      duration: 3000,
-    });
-  }, [isOffline, setOfflineAtom]);
-
-  return { setOnline, setOffline };
+  return { setOffline };
 };
 
 export const OfflineGuard = () => {
-  const { setOffline, setOnline } = useOfflineStatus();
+  const { setOffline } = useOfflineStatus();
 
   useEffect(() => {
     const handleOffline = () => {
       setOffline();
     };
 
-    const handleOnline = () => {
-      setOnline();
-    };
-
-    window.addEventListener("online", handleOnline);
     window.addEventListener("offline", handleOffline);
 
     return () => {
       window.removeEventListener("offline", handleOffline);
-      window.removeEventListener("online", handleOnline);
     };
-  }, [setOffline, setOnline]);
+  }, [setOffline]);
 
   return null;
 };
