69809a46e88759781ed3a90d3656d98855c35a9a
Sam Gathercole <sapagat@fastmail.com>
2025-06-12 12:14:46 +0200

    Hide footer items as it gets smaller

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,27 +7,43 @@ import {
 } from "@radix-ui/react-icons";
 import { useAtomValue } from "jotai";
 import { useMemo } from "react";
+import { useBreakpoint } from "src/hooks/use-breakpoint";
+import { isFeatureOn } from "src/infra/feature-flags";
 import { translate } from "src/infra/i18n";
 import { localizeDecimal } from "src/infra/i18n/numbers";
 import { dataAtom, simulationAtom } from "src/state/jotai";
 
 export const Footer = () => {
   const { hydraulicModel, modelMetadata } = useAtomValue(dataAtom);
+  const isLgOrLarger = useBreakpoint("lg");
+  const isMdOrLarger = useBreakpoint("md");
 
   const items: string[] = useMemo(
-    () => [
-      `${translate("autoLengths")}: ${translate("on")}`,
-      `${translate("autoElevations")}: ${translate("on")}`,
-      `${translate("units")}: ${modelMetadata.quantities.specName}`,
-      `${translate("headlossShort")}: ${hydraulicModel.headlossFormula}`,
-      `${translate("demandMultiplier")}: ${localizeDecimal(hydraulicModel.demands.multiplier)}`,
-    ],
-    [hydraulicModel, modelMetadata],
+    () =>
+      !isFeatureOn("FLAG_RESPONSIVE") || isLgOrLarger
+        ? [
+            `${translate("autoLengths")}: ${translate("on")}`,
+            `${translate("autoElevations")}: ${translate("on")}`,
+            `${translate("units")}: ${modelMetadata.quantities.specName}`,
+            `${translate("headlossShort")}: ${hydraulicModel.headlossFormula}`,
+            `${translate("demandMultiplier")}: ${localizeDecimal(hydraulicModel.demands.multiplier)}`,
+          ]
+        : isMdOrLarger
+          ? [
+              `${translate("units")}: ${modelMetadata.quantities.specName}`,
+              `${translate("headlossShort")}: ${hydraulicModel.headlossFormula}`,
+              `${translate("demandMultiplier")}: ${localizeDecimal(hydraulicModel.demands.multiplier)}`,
+            ]
+          : [
+              `${translate("demandMultiplier")}: ${localizeDecimal(hydraulicModel.demands.multiplier)}`,
+            ],
+    [hydraulicModel, modelMetadata, isLgOrLarger, isMdOrLarger],
   );
 
   return (
     <nav className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-md ">
       <div className="flex flex-row items-center text-sm text-gray-500 space-x-1">
+        <div className="border-r-2 border-gray-100 h-10"></div>
         {items.map((item, i) => (
           <>
             <span key={i} className="px-4 py-2">
