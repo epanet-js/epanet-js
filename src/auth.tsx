dee6d58310cfeb07ee2bec4d46d20bb0dc9cceb0
Sam Gathercole <sapagat@fastmail.com>
2025-06-18 18:24:02 +0200

    Disable auth when not configured

--- src/auth.tsx
+++ src/auth.tsx
@@ -19,12 +19,18 @@ import { translate } from "./infra/i18n";
 import { nullUser, User, UseAuthHook } from "./auth-types";
 import { Plan } from "./user-plan";
 
+const isAuthEnabled = !!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const handleError = useCallback((error: Error) => {
     captureWarning(error.message);
   }, []);
 
   const clerkLocalization = getLocale() === "es" ? esES : enUS;
+
+  if (!isAuthEnabled) {
+    return children as JSX.Element;
+  }
 
   return (
     // @ts-expect-error need to fix @types/react https://github.com/reduxjs/react-redux/issues/1886
@@ -34,7 +40,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export const useAuth: UseAuthHook = () => {
+const useAuthWithClerk: UseAuthHook = () => {
   const { isSignedIn, userId, signOut } = useClerkAuth();
   const { user: clerkUser } = useClerkUser();
 
@@ -51,24 +57,38 @@ export const useAuth: UseAuthHook = () => {
   return { isSignedIn, userId, user, signOut };
 };
 
+const useAuthNull: UseAuthHook = () => {
+  return {
+    isSignedIn: false,
+    userId: undefined,
+    user: nullUser,
+    signOut: () => {},
+  };
+};
+
+export const useAuth = isAuthEnabled ? useAuthWithClerk : useAuthNull;
+
 export const SignInButton = ({
   onClick,
   autoFocus = false,
 }: {
   onClick?: () => void;
   autoFocus?: boolean;
-}) => (
-  <ClerkSignInButton>
-    <Button
-      variant="quiet"
-      className="text-purple-500 font-semibold"
-      autoFocus={autoFocus}
-      onClick={onClick}
-    >
-      {translate("login")}
-    </Button>
-  </ClerkSignInButton>
-);
+}) => {
+  if (!isAuthEnabled) return null;
+  return (
+    <ClerkSignInButton>
+      <Button
+        variant="quiet"
+        className="text-purple-500 font-semibold"
+        autoFocus={autoFocus}
+        onClick={onClick}
+      >
+        {translate("login")}
+      </Button>
+    </ClerkSignInButton>
+  );
+};
 
 export const SignUpButton = ({
   onClick,
@@ -76,22 +96,34 @@ export const SignUpButton = ({
 }: {
   onClick?: () => void;
   autoFocus?: boolean;
-}) => (
-  <ClerkSignUpButton>
-    <Button variant="primary" onClick={onClick} autoFocus={autoFocus}>
-      <PersonIcon /> {translate("register")}
-    </Button>
-  </ClerkSignUpButton>
-);
+}) => {
+  if (!isAuthEnabled) return null;
+
+  return (
+    <ClerkSignUpButton>
+      <Button variant="primary" onClick={onClick} autoFocus={autoFocus}>
+        <PersonIcon /> {translate("register")}
+      </Button>
+    </ClerkSignUpButton>
+  );
+};
 
 export const SignedIn = ({ children }: { children: React.ReactNode }) => {
+  if (!isAuthEnabled) return null;
   // @ts-expect-error need to fix @types/react https://github.com/reduxjs/react-redux/issues/1886
   return <ClerkSignedIn>{children}</ClerkSignedIn>;
 };
 export const SignedOut = ({ children }: { children: React.ReactNode }) => {
+  if (!isAuthEnabled) return children as JSX.Element;
   // @ts-expect-error need to fix @types/react https://github.com/reduxjs/react-redux/issues/1886
   return <ClerkSignedOut>{children}</ClerkSignedOut>;
 };
-export const UserButton = ClerkUserButton;
+export const UserButton = isAuthEnabled
+  ? ClerkUserButton
+  : () => <button></button>;
 
-export const RedirectToSignIn = ClerkRedirectToSignIn;
+export const RedirectToSignIn = isAuthEnabled
+  ? ClerkRedirectToSignIn
+  : () => {
+      return null;
+    };
