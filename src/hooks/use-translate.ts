a92c708de318c5b5af183ee6c9bc8d7f9f44171e
Sam Gathercole <sapagat@fastmail.com>
2025-07-05 10:16:48 +0200

    Add FLAG_I18N

--- src/hooks/use-translate.ts
+++ src/hooks/use-translate.ts
@@ -3,6 +3,8 @@ import { localeAtom } from "src/state/locale";
 import { Translations } from "src/infra/i18n/locales/locale";
 import { locales } from "src/infra/i18n/locale";
 import { captureError } from "src/infra/error-tracking";
+import { useFeatureFlag } from "src/hooks/use-feature-flags";
+import { useCallback } from "react";
 
 const compileText = (template: string, variables: string[]): string => {
   let result = template;
@@ -14,16 +16,37 @@ const compileText = (template: string, variables: string[]): string => {
 
 export const useTranslate = () => {
   const locale = useAtomValue(localeAtom);
+  const isI18NextOn = useFeatureFlag("FLAG_I18NEXT");
 
-  return (key: string, ...variables: string[]): string => {
-    const translations = locales[locale].translations;
-    const template = translations[key as keyof Translations];
-    if (!template) {
-      captureError(new Error(`Missing translation for ${key}`));
-      return key;
-    }
+  const translate = useCallback(
+    (key: string, ...variables: string[]): string => {
+      const translations = locales[locale].translations;
+      const template = translations[key as keyof Translations];
+      if (!template) {
+        captureError(new Error(`Missing translation for ${key}`));
+        return key;
+      }
 
-    const text = compileText(template, variables);
-    return text;
-  };
+      const text = compileText(template, variables);
+      return text;
+    },
+    [locale],
+  );
+
+  const translateDeprecated = useCallback(
+    (key: string, ...variables: string[]): string => {
+      const translations = locales[locale].translations;
+      const template = translations[key as keyof Translations];
+      if (!template) {
+        captureError(new Error(`Missing translation for ${key}`));
+        return key;
+      }
+
+      const text = compileText(template, variables);
+      return text;
+    },
+    [locale],
+  );
+
+  return isI18NextOn ? translate : translateDeprecated;
 };
