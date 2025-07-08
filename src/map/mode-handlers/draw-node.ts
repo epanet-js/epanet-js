dba8e7bafaead9f5a25856762185f2f18506c3aa
Sam Gathercole <sapagat@fastmail.com>
2025-07-08 08:44:42 +0200

    Extract generic draw node handlers

--- src/map/mode-handlers/draw-node.ts
+++ src/map/mode-handlers/draw-node.ts
@@ -10,16 +10,22 @@ import noop from "lodash/noop";
 import { useSetAtom } from "jotai";
 import { CURSOR_DEFAULT } from "src/lib/constants";
 import { getMapCoord } from "./utils";
-import { addJunction } from "src/hydraulic-model/model-operations";
+import {
+  addJunction,
+  addReservoir,
+} from "src/hydraulic-model/model-operations";
 import throttle from "lodash/throttle";
 import { useUserTracking } from "src/infra/user-tracking";
 import { useElevations } from "../elevations/use-elevations";
 
-export function useJunctionHandlers({
+type NodeType = "junction" | "reservoir";
+
+export function useDrawNodeHandlers({
   mode,
   hydraulicModel,
   rep,
-}: HandlerContext): Handlers {
+  nodeType,
+}: HandlerContext & { nodeType: NodeType }): Handlers {
   const setSelection = useSetAtom(selectionAtom);
   const setMode = useSetAtom(modeAtom);
   const setCursor = useSetAtom(cursorStyleAtom);
@@ -37,16 +43,33 @@ export function useJunctionHandlers({
 
       const clickPosition = getMapCoord(e);
       const elevation = await fetchElevation(e.lngLat);
-      const junction = assetBuilder.buildJunction({
-        elevation,
-        coordinates: clickPosition,
-      });
 
-      const id = junction.id;
+      let node;
+      let moment;
 
-      const moment = addJunction(hydraulicModel, { junction });
+      switch (nodeType) {
+        case "junction":
+          node = assetBuilder.buildJunction({
+            elevation,
+            coordinates: clickPosition,
+          });
+          moment = addJunction(hydraulicModel, { junction: node });
+          break;
+        case "reservoir":
+          node = assetBuilder.buildReservoir({
+            elevation,
+            coordinates: clickPosition,
+          });
+          moment = addReservoir(hydraulicModel, { reservoir: node });
+          break;
+        default:
+          throw new Error(`Unsupported node type: ${nodeType as string}`);
+      }
+
+      const id = node.id;
+
       transact(moment);
-      userTracking.capture({ name: "asset.created", type: "junction" });
+      userTracking.capture({ name: "asset.created", type: nodeType });
       if (!multi) {
         setSelection(USelection.single(id));
       }
--- src/map/mode-handlers/index.ts
+++ src/map/mode-handlers/index.ts
@@ -1,15 +1,20 @@
 import type { HandlerContext } from "src/types";
 import { Mode } from "src/state/jotai";
 import { useNoneHandlers } from "./none";
-import { useJunctionHandlers } from "./junction";
-import { useDrawReservoirHandlers } from "./draw-reservoir";
+import { useDrawNodeHandlers } from "./draw-node";
 import { useDrawLinkHandlers } from "./draw-link";
 
 export function useModeHandlers(handlerContext: HandlerContext) {
   const HANDLERS: Record<Mode, Handlers> = {
     [Mode.NONE]: useNoneHandlers(handlerContext),
-    [Mode.DRAW_JUNCTION]: useJunctionHandlers(handlerContext),
-    [Mode.DRAW_RESERVOIR]: useDrawReservoirHandlers(handlerContext),
+    [Mode.DRAW_JUNCTION]: useDrawNodeHandlers({
+      ...handlerContext,
+      nodeType: "junction",
+    }),
+    [Mode.DRAW_RESERVOIR]: useDrawNodeHandlers({
+      ...handlerContext,
+      nodeType: "reservoir",
+    }),
     [Mode.DRAW_PIPE]: useDrawLinkHandlers({
       ...handlerContext,
       linkType: "pipe",
