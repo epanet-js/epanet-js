890bfda7b79d65872ee299bbf6620d5e15d529ab
Sam Gathercole <sapagat@fastmail.com>
2025-07-08 09:26:09 +0200

    Use generic add node from drawing handlers

---
+++ src/hydraulic-model/model-operations/add-node.ts
@@ -0,0 +1,26 @@
+import { NodeAsset } from "../asset-types";
+import { ModelOperation } from "../model-operation";
+import { LabelGenerator } from "../label-manager";
+
+type InputData = {
+  node: NodeAsset;
+};
+
+export const addNode: ModelOperation<InputData> = (
+  hydraulicModel,
+  { node },
+) => {
+  const nodeCopy = node.copy();
+  addMissingLabel(hydraulicModel.labelManager, nodeCopy);
+
+  return {
+    note: `Add ${node.type}`,
+    putAssets: [nodeCopy],
+  };
+};
+
+const addMissingLabel = (labelGenerator: LabelGenerator, node: NodeAsset) => {
+  if (node.label === "") {
+    node.setProperty("label", labelGenerator.generateFor(node.type, node.id));
+  }
+};
--- src/map/mode-handlers/draw-node/draw-node-handlers.ts
+++ src/map/mode-handlers/draw-node/draw-node-handlers.ts
@@ -1,51 +1,41 @@
-import { USelection } from "src/selection";
 import type { HandlerContext } from "src/types";
-import {
-  modeAtom,
-  Mode,
-  selectionAtom,
-  cursorStyleAtom,
-} from "src/state/jotai";
+import { modeAtom, Mode, cursorStyleAtom } from "src/state/jotai";
 import noop from "lodash/noop";
 import { useSetAtom } from "jotai";
 import { CURSOR_DEFAULT } from "src/lib/constants";
-import { getMapCoord } from "./utils";
-import {
-  addJunction,
-  addReservoir,
-} from "src/hydraulic-model/model-operations";
+import { getMapCoord } from "../utils";
+import { addNode } from "src/hydraulic-model/model-operations/add-node";
 import throttle from "lodash/throttle";
 import { useUserTracking } from "src/infra/user-tracking";
-import { useElevations } from "../elevations/use-elevations";
+import { useElevations } from "../../elevations/use-elevations";
+import { NodeAsset } from "src/hydraulic-model";
 
 type NodeType = "junction" | "reservoir";
 
 export function useDrawNodeHandlers({
-  mode,
   hydraulicModel,
   rep,
   nodeType,
 }: HandlerContext & { nodeType: NodeType }): Handlers {
-  const setSelection = useSetAtom(selectionAtom);
   const setMode = useSetAtom(modeAtom);
   const setCursor = useSetAtom(cursorStyleAtom);
   const transact = rep.useTransact();
   const userTracking = useUserTracking();
-  const multi = mode.modeOptions?.multi;
   const { assetBuilder, units } = hydraulicModel;
   const { fetchElevation, prefetchTile } = useElevations(units.elevation);
 
+  const submitNode = (node: NodeAsset) => {
+    const moment = addNode(hydraulicModel, { node });
+    transact(moment);
+    userTracking.capture({ name: "asset.created", type: nodeType });
+  };
+
   return {
     click: async (e) => {
-      if (!multi) {
-        setMode({ mode: Mode.NONE });
-      }
-
       const clickPosition = getMapCoord(e);
       const elevation = await fetchElevation(e.lngLat);
 
       let node;
-      let moment;
 
       switch (nodeType) {
         case "junction":
@@ -53,26 +43,18 @@ export function useDrawNodeHandlers({
             elevation,
             coordinates: clickPosition,
           });
-          moment = addJunction(hydraulicModel, { junction: node });
           break;
         case "reservoir":
           node = assetBuilder.buildReservoir({
             elevation,
             coordinates: clickPosition,
           });
-          moment = addReservoir(hydraulicModel, { reservoir: node });
           break;
         default:
           throw new Error(`Unsupported node type: ${nodeType as string}`);
       }
 
-      const id = node.id;
-
-      transact(moment);
-      userTracking.capture({ name: "asset.created", type: nodeType });
-      if (!multi) {
-        setSelection(USelection.single(id));
-      }
+      submitNode(node);
     },
     move: throttle((e) => {
       prefetchTile(e.lngLat);
