943cce0bbe595014efbe5c3c9baf1a386acdfc09
Sam Gathercole <sapagat@fastmail.com>
2025-07-08 10:01:24 +0200

    Remove unused model operations

--- src/hydraulic-model/model-operations/index.ts
+++ src/hydraulic-model/model-operations/index.ts
@@ -1,8 +1,5 @@
 export { deleteAssets } from "./delete-assets";
-export { addPipe } from "./add-pipe";
 export { addLink } from "./add-link";
-export { addJunction } from "./add-junction";
-export { addReservoir } from "./add-reservoir";
 export { moveNode } from "./move-node";
 export { changePipeStatus } from "./change-pipe-status";
 export { changeProperty } from "./change-property";
