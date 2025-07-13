66cb80ff2c28f6806a79521f16bc4a6e96e4fddb
Sam Gathercole <sapagat@fastmail.com>
2025-07-13 02:00:45 +0200

    Remove unused libs

--- src/lib/detect-compatibility.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-
-import { detectCompatibility } from "./detect-compatibility";
-
-test("detectCompatibility", () => {
-  expect(detectCompatibility()).toBeFalsy();
-});
