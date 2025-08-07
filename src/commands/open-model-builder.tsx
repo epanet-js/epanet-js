0ce5677ab25592e0f7c418ea02f303bf6cf9d747
Sam Gathercole <sapagat@fastmail.com>
2025-08-07 14:21:17 +0200

    Remove FLAG_MODEL_BUILD_IFRAME

--- src/commands/open-model-builder.tsx
+++ src/commands/open-model-builder.tsx
@@ -1,14 +1,11 @@
 import { useCallback } from "react";
 import { useUserTracking } from "src/infra/user-tracking";
-import { useFeatureFlag } from "src/hooks/use-feature-flags";
 import { useSetAtom } from "jotai";
 import { dialogAtom } from "src/state/dialog";
-import { modelBuilderUrl } from "src/global-config";
 import { useEarlyAccess } from "src/hooks/use-early-access";
 
 export const useOpenModelBuilder = () => {
   const userTracking = useUserTracking();
-  const isModelBuildIframeOn = useFeatureFlag("FLAG_MODEL_BUILD_IFRAME");
   const setDialogState = useSetAtom(dialogAtom);
   const onlyEarlyAccess = useEarlyAccess();
 
@@ -20,14 +17,10 @@ export const useOpenModelBuilder = () => {
           source,
         });
 
-        if (isModelBuildIframeOn) {
-          setDialogState({ type: "modelBuilderIframe" });
-        } else {
-          window.open(modelBuilderUrl, "_blank");
-        }
+        setDialogState({ type: "modelBuilderIframe" });
       }, "modelBuilderIframe");
     },
-    [userTracking, isModelBuildIframeOn, setDialogState, onlyEarlyAccess],
+    [userTracking, setDialogState, onlyEarlyAccess],
   );
 
   return openModelBuilder;
