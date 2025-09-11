405d37a512a2fb9441fe2127da39f5123acbb3b7
Sam Gathercole <sapagat@fastmail.com>
2025-09-11 12:31:39 +0200

    Mark as deprecated draw link handlers

--- src/map/mode-handlers/draw-link/index.ts
+++ src/map/mode-handlers/draw-link/index.ts
@@ -1,2 +1,3 @@
 export { useDrawLinkHandlers } from "./draw-link-handlers";
+export { useDrawLinkHandlersDeprecated } from "./draw-link-handlers-deprecated";
 export type { EphemeralDrawLink } from "./ephemeral-link-state";
