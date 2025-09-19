6954234a9216803355c08fc9a25b5465d7c24490
Sam Gathercole <sapagat@fastmail.com>
2025-09-19 08:55:33 +0200

    Fallback to default handlers from edit vertices

--- src/map/mode-handlers/edit-vertices/index.ts
+++ src/map/mode-handlers/edit-vertices/index.ts
@@ -1,35 +1,18 @@
 import type { HandlerContext } from "src/types";
 import { Mode, ephemeralStateAtom, modeAtom } from "src/state/jotai";
 import { useSetAtom, useAtom } from "jotai";
-import throttle from "lodash/throttle";
 import { useSelection } from "src/selection";
-import { QueryProvider, getClickedFeature } from "src/map/fuzzy-click";
-import { decodeId } from "src/lib/id";
-import { UIDMap } from "src/lib/id-mapper";
-import { Asset } from "src/hydraulic-model";
+import { useNoneHandlers } from "../none/none-handlers";
 
 export function useEditVerticesHandlers(
   handlerContext: HandlerContext,
 ): Handlers {
-  const { selection, idMap, map, hydraulicModel } = handlerContext;
+  const { selection } = handlerContext;
   const setMode = useSetAtom(modeAtom);
   const { clearSelection } = useSelection(selection);
-  const [ephemeralState, setEphemeralState] = useAtom(ephemeralStateAtom);
+  const [, setEphemeralState] = useAtom(ephemeralStateAtom);
 
-  const getClickedAsset = (
-    e: mapboxgl.MapMouseEvent | mapboxgl.MapTouchEvent,
-  ): Asset | null => {
-    const rawId = getClickedFeature(map as QueryProvider, e.point);
-    if (rawId === null) return null;
-
-    const decodedId = decodeId(rawId);
-    const uuid = UIDMap.getUUID(idMap, decodedId.featureId);
-
-    const asset = hydraulicModel.assets.get(uuid);
-    if (!asset) return null;
-
-    return asset;
-  };
+  const defaultHandlers = useNoneHandlers(handlerContext);
 
   const exitEditVerticesMode = () => {
     clearSelection();
@@ -37,28 +20,14 @@ export function useEditVerticesHandlers(
     setMode({ mode: Mode.NONE });
   };
 
-  const move = throttle(
-    (_e: mapboxgl.MapMouseEvent | mapboxgl.MapTouchEvent) => {},
-    16,
-  );
-
-  const click = (e: mapboxgl.MapMouseEvent | mapboxgl.MapTouchEvent) => {
-    if (ephemeralState.type !== "editVertices") {
-      return;
-    }
-
-    const clickedAsset = getClickedAsset(e);
-    const currentLinkId = ephemeralState.linkId;
-
-    if (!clickedAsset || clickedAsset.id !== currentLinkId) {
-      exitEditVerticesMode();
-    }
-  };
-
   const handlers: Handlers = {
-    click,
+    click: (e) => {
+      setMode({ mode: Mode.NONE });
+      setEphemeralState({ type: "none" });
+      defaultHandlers.click(e);
+    },
     double: () => {},
-    move,
+    move: defaultHandlers.move,
     down: () => {},
     up: () => {},
     exit: exitEditVerticesMode,
