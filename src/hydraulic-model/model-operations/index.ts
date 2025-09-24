372c11c9e5cc891f4f4e71cb2dcde5f02736dd73
Sam Gathercole <sapagat@fastmail.com>
2025-09-24 12:21:30 +0200

    Remove deprecated add link operation

--- src/hydraulic-model/model-operations/index.ts
+++ src/hydraulic-model/model-operations/index.ts
@@ -1,6 +1,5 @@
 export { deleteAssets } from "./delete-assets";
 export { addLink } from "./add-link";
-export { addLinkDeprecated } from "./add-link-deprecated";
 export { moveNode } from "./move-node";
 export { changeProperty } from "./change-property";
 export { allocateCustomerPoints } from "./allocate-customer-points";
