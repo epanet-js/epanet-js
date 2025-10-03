dc53e36ef405a6ce8e3a72e7a40b853c24398d75
Sam Gathercole <sapagat@fastmail.com>
2025-10-03 12:38:02 +0200

    Remove divider in map styling editor

--- src/components/panels/map-styling-editor.tsx
+++ src/components/panels/map-styling-editor.tsx
@@ -39,7 +39,7 @@ export const MapStylingEditor = () => {
 
   return (
     <div className="flex-auto overflow-y-auto placemark-scrollbar">
-      <div className="flex flex-col divide-y divide-gray-200 dark:divide-gray-900 border-gray-200 dark:border-gray-900">
+      <div className="flex flex-col divide-gray-200 border-gray-200 dark:border-gray-900">
         <SymbologyEditor
           geometryType="node"
           properties={supportedNodeProperties}
