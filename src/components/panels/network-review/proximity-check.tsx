163eebd58d3e894477217c9412f5991cd574068f
Sandra Saornil <sandra@iterating.ca>
2025-10-16 08:07:00 +0200

    Display proximity anomalies results in network view panel

--- src/components/panels/network-review/proximity-check.tsx
+++ src/components/panels/network-review/proximity-check.tsx
@@ -2,25 +2,109 @@ import { NumericField } from "src/components/form/numeric-field";
 import { localizeDecimal } from "src/infra/i18n/numbers";
 
 import { CheckType, EmptyState, ToolDescription, ToolHeader } from "./common";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useAtomValue } from "jotai";
-import { dataAtom } from "src/state/jotai";
+import { dataAtom, selectionAtom } from "src/state/jotai";
 import { useTranslate } from "src/hooks/use-translate";
-import { convertTo } from "src/quantity";
+import { convertTo, Quantity } from "src/quantity";
+import {
+  PossibleConnection,
+  findPossibleConnections,
+} from "src/lib/network-review/proximity-check";
+import { useSelection, USelection } from "src/selection";
+import { useZoomTo } from "src/hooks/use-zoom-to";
+import { useUserTracking } from "src/infra/user-tracking";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { Button } from "src/components/elements";
+import { JunctionIcon, PipeIcon, ReservoirIcon, TankIcon } from "src/icons";
+import { AssetType, Pipe } from "src/hydraulic-model";
 
 export const ProximityCheck = ({ onGoBack }: { onGoBack: () => void }) => {
-  const findJunctionsCloserThan = useCallback(() => {}, []);
+  const userTracking = useUserTracking();
+  const { checkPossibleConnections, possibleConnections } =
+    useCheckNearbyConnections();
+  const { distanceInM, localizedDistance, updateDistance } = useDistance();
+  const selection = useAtomValue(selectionAtom);
+  const { setSelection, isSelected } = useSelection(selection);
+  const zoomTo = useZoomTo();
+  const { hydraulicModel } = useAtomValue(dataAtom);
+  const [selectedConnectionId, setSelectedConnectionId] = useState<
+    string | null
+  >(null);
+
+  const lastIssuesCount = useRef(0);
+
+  useEffect(
+    function recomputePossibleConnections() {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      checkPossibleConnections(distanceInM);
+    },
+    [distanceInM, checkPossibleConnections],
+  );
+
+  const selectPossibleConnection = useCallback(
+    (connection: PossibleConnection) => {
+      const nodeAsset = hydraulicModel.assets.get(connection.nodeId);
+      const pipeAsset = hydraulicModel.assets.get(connection.pipeId);
+      if (!nodeAsset || !pipeAsset) {
+        setSelectedConnectionId(null);
+        return;
+      }
+      const connectionId = `${connection.nodeId}-${connection.pipeId}`;
+      setSelectedConnectionId(connectionId);
+      setSelection(USelection.fromIds([connection.nodeId, connection.pipeId]));
+      zoomTo([nodeAsset]);
+    },
+    [hydraulicModel, setSelection, zoomTo],
+  );
+
+  useEffect(() => {
+    const selectedConnection = possibleConnections.find(
+      (connection) =>
+        isSelected(connection.nodeId) && isSelected(connection.pipeId),
+    );
+
+    if (!selectedConnection) {
+      setSelectedConnectionId(null);
+    } else {
+      const connectionId = `${selectedConnection.nodeId}-${selectedConnection.pipeId}`;
+      setSelectedConnectionId((prev) =>
+        prev === connectionId ? prev : connectionId,
+      );
+    }
+  }, [possibleConnections, isSelected]);
+
+  useEffect(() => {
+    const issuesCount = possibleConnections.length;
+    if (lastIssuesCount.current !== issuesCount) {
+      lastIssuesCount.current = issuesCount;
+      userTracking.capture({
+        name: "networkReview.proximityCheck.changed",
+        count: issuesCount,
+      });
+    }
+  }, [possibleConnections, userTracking]);
 
   return (
     <div className="absolute inset-0 flex flex-col">
       <ToolHeader
         onGoBack={onGoBack}
-        itemsCount={0}
+        itemsCount={possibleConnections.length}
         checkType={CheckType.proximityCheck}
       />
-      <ToolDescription checkType={CheckType.proximityCheck} />
-      <DistanceInput onChange={findJunctionsCloserThan} />
-      <EmptyState checkType={CheckType.proximityCheck} />
+      <DistanceInput distance={localizedDistance} onChange={updateDistance} />
+      {possibleConnections.length > 0 ? (
+        <IssuesList
+          issues={possibleConnections}
+          onClick={selectPossibleConnection}
+          selectedId={selectedConnectionId}
+        />
+      ) : (
+        <>
+          <ToolDescription checkType={CheckType.proximityCheck} />
+          <EmptyState checkType={CheckType.proximityCheck} />
+        </>
+      )}
     </div>
   );
 };
@@ -30,10 +114,31 @@ const DEFAULT_DISTANCE_M = 0.5;
 
 const DistanceInput = ({
   onChange,
+  distance,
 }: {
   onChange: (distance: number) => void;
+  distance: Quantity;
 }) => {
   const translate = useTranslate();
+
+  const label = `${translate("networkReview.proximityCheck.distance")} (${distance.unit})`;
+
+  return (
+    <div className="flex gap-2 flex-row p-3 items-center">
+      <label className="pr-2 text-sm flex-1">{label}</label>
+      <div className="flex-1">
+        <NumericField
+          label={label}
+          displayValue={localizeDecimal(distance.value)}
+          onChangeValue={onChange}
+          styleOptions={{ padding: "sm", textSize: "sm" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+const useDistance = () => {
   const {
     hydraulicModel: {
       units: { length: unit },
@@ -42,29 +147,215 @@ const DistanceInput = ({
   const [distance, setDistance] = useState<number>(() =>
     unit === "ft" ? DEFAULT_DISTANCE_FT : DEFAULT_DISTANCE_M,
   );
+  const distanceInM = useRef<number>(convertTo({ value: distance, unit }, "m"));
 
-  const label = `${translate("networkReview.proximityCheck.distance")} (${unit})`;
-
-  const reportDistanceChange = useCallback(
+  const updateDistance = useCallback(
     (value: number) => {
-      const distanceInM = convertTo({ value, unit }, "m");
       setDistance(value);
-      onChange(distanceInM);
+      distanceInM.current = convertTo({ value, unit }, "m");
     },
-    [onChange, unit],
+    [unit],
   );
 
+  return {
+    distanceInM: distanceInM.current,
+    localizedDistance: { value: distance, unit },
+    updateDistance,
+  };
+};
+
+const useCheckNearbyConnections = () => {
+  const [possibleConnections, setPossibleConnections] = useState<
+    PossibleConnection[]
+  >([]);
+  const { hydraulicModel } = useAtomValue(dataAtom);
+
+  const checkPossibleConnections = useCallback(
+    async (distance: number) => {
+      const result = await findPossibleConnections(hydraulicModel, distance);
+      setPossibleConnections(result);
+    },
+    [hydraulicModel],
+  );
+
+  return { checkPossibleConnections, possibleConnections };
+};
+
+const IssuesList = ({
+  issues,
+  onClick,
+  selectedId,
+}: {
+  issues: PossibleConnection[];
+  onClick: (issue: PossibleConnection) => void;
+  selectedId: string | null;
+}) => {
+  const headerRows = 1;
+  const parentRef = useRef(null);
+  const rowVirtualizer = useVirtualizer({
+    count: issues.length + headerRows,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 35,
+  });
+
+  useEffect(() => {
+    if (selectedId === null) return;
+
+    const rowIndex =
+      issues.findIndex(
+        (connection) =>
+          `${connection.nodeId}-${connection.pipeId}` === selectedId,
+      ) + headerRows;
+
+    const range = rowVirtualizer.range;
+
+    if (!range) return;
+    const { startIndex, endIndex } = range;
+    if (rowIndex >= startIndex && rowIndex < endIndex) {
+      return;
+    }
+
+    rowVirtualizer.scrollToIndex(rowIndex, {
+      align: "center",
+    });
+  }, [selectedId, issues, rowVirtualizer]);
+
+  const items = rowVirtualizer.getVirtualItems();
+
   return (
-    <div className="flex gap-2 flex-row p-3 items-center">
-      <label className="pr-2 text-sm flex-1">{label}</label>
-      <div className="flex-1">
-        <NumericField
-          label={label}
-          displayValue={localizeDecimal(distance)}
-          onChangeValue={reportDistanceChange}
-          styleOptions={{ padding: "sm", textSize: "sm" }}
-        />
+    <div
+      ref={parentRef}
+      className="flex-auto p-1 overflow-y-auto placemark-scrollbar"
+      style={{ contain: "strict" }}
+      tabIndex={0}
+    >
+      <div
+        className="w-full relative"
+        style={{ height: rowVirtualizer.getTotalSize() }}
+      >
+        <div
+          className="absolute top-0 left-0 w-full"
+          style={{
+            transform: `translateY(${items[0]?.start ?? 0}px)`,
+          }}
+        >
+          {items.map((virtualRow) => {
+            if (virtualRow.index === 0) {
+              return (
+                <div
+                  key="description"
+                  data-index={virtualRow.index}
+                  className="w-full"
+                  ref={rowVirtualizer.measureElement}
+                  role="listItem"
+                >
+                  <ToolDescription checkType={CheckType.proximityCheck} />
+                </div>
+              );
+            }
+
+            const issue = issues[virtualRow.index - headerRows];
+            return (
+              <div
+                key={`${issue.nodeId}-${issue.pipeId}`}
+                data-index={virtualRow.index}
+                className="w-full"
+                ref={rowVirtualizer.measureElement}
+                role="listItem"
+              >
+                <PossibleConnectionItem
+                  connection={issue}
+                  selectedId={selectedId}
+                  onClick={onClick}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
+  );
+};
+
+const iconByAssetType: { [key in AssetType]: React.ReactNode } = {
+  junction: <JunctionIcon />,
+  tank: <TankIcon />,
+  reservoir: <ReservoirIcon />,
+  valve: <PipeIcon />,
+  pump: <PipeIcon />,
+  pipe: <PipeIcon />,
+};
+
+const PossibleConnectionItem = ({
+  connection,
+  onClick,
+  selectedId,
+}: {
+  connection: PossibleConnection;
+  onClick: (connection: PossibleConnection) => void;
+  selectedId: string | null;
+}) => {
+  const translate = useTranslate();
+  const { hydraulicModel } = useAtomValue(dataAtom);
+  const connectionId = `${connection.nodeId}-${connection.pipeId}`;
+  const isSelected = selectedId === connectionId;
+
+  const nodeAsset = hydraulicModel.assets.get(connection.nodeId);
+  const pipeAsset = hydraulicModel.assets.get(connection.pipeId);
+
+  if (!nodeAsset || !pipeAsset || pipeAsset.type !== "pipe") return null;
+
+  const pipe = pipeAsset as Pipe;
+  const lengthUnit = hydraulicModel.units.length;
+  const distanceInModelUnits = convertTo(
+    { value: connection.distance, unit: "m" },
+    lengthUnit,
+  );
+  const distanceFormatted = localizeDecimal(distanceInModelUnits, {
+    decimals: 2,
+  });
+  const diameterFormatted = localizeDecimal(pipe.diameter);
+
+  return (
+    <Button
+      onClick={() => onClick(connection)}
+      variant={"quiet"}
+      role="button"
+      aria-label={translate(
+        "networkReview.proximityCheck.issueLabel",
+        nodeAsset.label,
+        pipeAsset.label,
+        distanceFormatted,
+      )}
+      aria-checked={isSelected}
+      aria-expanded={isSelected ? "true" : "false"}
+      className="group w-full"
+    >
+      <div
+        className="grid gap-x-2 items-start p-1 pr-0 text-sm w-full"
+        style={{
+          gridTemplateColumns: "auto 1fr auto",
+        }}
+      >
+        <div className="pt-[.125rem] flex-shrink-0">
+          {iconByAssetType[nodeAsset.type]}
+        </div>
+        <div className="text-left min-w-0">
+          <div className="text-sm font-semibold truncate">
+            {nodeAsset.label}
+          </div>
+          <div className="text-xs text-gray-500 flex items-center gap-1 min-w-0">
+            <PipeIcon size={12} className="flex-shrink-0" />
+            <span className="truncate">{pipeAsset.label}</span>
+            <span className="flex-shrink-0 whitespace-nowrap">
+              ⌀ {diameterFormatted}
+            </span>
+          </div>
+        </div>
+        <div className="text-xs text-gray-500 pt-[.125rem] flex-shrink-0 whitespace-nowrap">
+          {distanceFormatted} {lengthUnit}
+        </div>
+      </div>
+    </Button>
   );
 };
