24b924cc36a53a6b5f5436fa035fe0bfe5274661
iteratingDesign <designteam@iterating.ca>
2025-11-12 09:54:18 +0100

    Checkbox: improve disabled style

--- src/components/form/Checkbox.tsx
+++ src/components/form/Checkbox.tsx
@@ -1,11 +1,19 @@
 export const Checkbox = ({
   size = 4,
+  disabled,
   ...props
 }: { size?: number } & React.InputHTMLAttributes<HTMLInputElement>) => {
   return (
     <input
       type="checkbox"
-      className={`cursor-pointer w-${size} h-${size} text-purple-400 bg-gray-200 border-gray-300 rounded focus:ring-purple-500`}
+      className={`
+        w-${size} h-${size} text-purple-400 border-gray-300 rounded
+        ${
+          disabled
+            ? "cursor-not-allowed bg-gray-200 opacity-50"
+            : "cursor-pointer bg-gray-50 focus:ring-purple-500"
+        }
+      `}
       {...props}
     />
   );
