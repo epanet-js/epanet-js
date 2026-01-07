3cc32a2054c27f7d42e101809bbdafaec82890a7
Sandra Saornil <sandra@iterating.ca>
2026-01-07 11:58:30 +0100

    Quick Graphs :: Add Echarts dependencies

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   compress: false,
   swcMinify: true,
   poweredByHeader: false,
+  transpilePackages: ["echarts", "zrender"],
 
   typescript: {
     ignoreBuildErrors: false,
