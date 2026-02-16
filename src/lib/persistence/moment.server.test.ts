4f75fb5b96c4824b8ae37ac956e6a8a8355a6708
Sandra Saornil <sandra@iterating.ca>
2026-02-16 16:12:19 +0100

    Cleanup unused persistence code

--- src/lib/persistence/moment.server.test.ts
+++ src/lib/persistence/moment.server.test.ts
@@ -1,6 +1,6 @@
 import { expect, describe, it } from "vitest";
 
-import { EMPTY_MOMENT, UMoment, fMoment } from "./moment";
+import { EMPTY_MOMENT, fMoment } from "./moment";
 
 describe("fMoment", () => {
   it("generates a moment", () => {
@@ -8,22 +8,5 @@ describe("fMoment", () => {
       ...EMPTY_MOMENT,
       note: "This is the note",
     });
-  });
-});
-
-describe("UMoment", () => {
-  it("#merge", () => {
-    const a = fMoment("This is the note");
-    const b = fMoment("Second moment");
-    a.deleteAssets.push(1);
-    b.deleteAssets.push(2);
-    const c = UMoment.merge(a, b);
-    expect(c).toHaveProperty("deleteAssets", [1, 2]);
-  });
-  it("#isEmpty", () => {
-    expect(UMoment.isEmpty(fMoment("This is the note"))).toBeTruthy();
-    const m = fMoment("This is the note");
-    m.deleteAssets.push(1);
-    expect(UMoment.isEmpty(m)).toBeFalsy();
   });
 });
--- src/lib/persistence/shared.ts
+++ src/lib/persistence/shared.ts
@@ -1,48 +1,13 @@
 import { IWrappedFeature, IFolder } from "src/types";
-import { fMoment, Moment, MomentInput } from "./moment";
 import { Data } from "src/state/jotai";
 import { isDebugOn } from "src/infra/debug-mode";
 import { HydraulicModel, ModelMoment } from "src/hydraulic-model";
-
-// This  used to send to posthog, but now could be removed
-// or wired into your own product analytics.
-export function trackMomentDeprecated(partialMoment: Partial<MomentInput>) {
-  const { track } = partialMoment;
-  if (isDebugOn) {
-    // eslint-disable-next-line no-console
-    console.log("TRANSACT_DEPRECATED", JSON.stringify(partialMoment));
-  }
-  if (track) {
-    delete partialMoment.track;
-  }
-}
 
 export function trackMoment(moment: ModelMoment) {
   if (isDebugOn) {
     // eslint-disable-next-line no-console
     console.log("TRANSACT", JSON.stringify(moment));
   }
-}
-/**
- * Same as momentForDeleteFolders but for features:
- * create an undelete operation.
- *
- * @param features The folders to delete by ID
- * @param hydraulicModel The hydraulic model
- * @returns a moment with an undelete
- */
-export function momentForDeleteFeatures(
-  features: readonly IWrappedFeature["id"][],
-  hydraulicModel: HydraulicModel,
-): Moment {
-  const moment = fMoment("Update features");
-  for (const id of features) {
-    const feature = hydraulicModel.assets.get(id);
-    if (feature) {
-      moment.putAssets.push(feature);
-    }
-  }
-  return moment;
 }
 
 function getLastAtInMap(map: Map<unknown, IFolder | IWrappedFeature>): string {
