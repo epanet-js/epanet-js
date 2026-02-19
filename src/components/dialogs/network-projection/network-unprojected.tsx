e35382d8999d105f30a9fb59ec9fd069cf93447e
iteratingDesign <designteam@iterating.ca>
2026-02-19 10:46:09 +0100

    Reprojection illustrations type fix

--- src/components/dialogs/network-projection/network-unprojected.tsx
+++ src/components/dialogs/network-projection/network-unprojected.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-const NetworkUnprojectedIllustration = (props) => (
+const NetworkUnprojectedIllustration = (
+  props: React.SVGProps<SVGSVGElement>,
+) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlSpace="preserve"
