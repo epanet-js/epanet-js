bdd781c78e13cd1e7da6ed3af1134b960f144d86
iteratingDesign <designteam@iterating.ca>
2026-02-19 10:36:20 +0100

    Reprojection: update pics

---
+++ src/components/dialogs/network-projection/network-projected.tsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+const NetworkProjectedIllustration = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    style={{
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      strokeMiterlimit: 1.5,
+    }}
+    viewBox="0 0 288 160"
+    {...props}
+  >
+    <path
+      d="M0 32h288M0 64h288M0 96h288M0 128h288M256 0v160M224 0v160M192 0v160M160 0v160M128 0v160M96 0v160M64 0v160M32 0v160"
+      style={{
+        fill: "none",
+        stroke: "#d4d4d4",
+        strokeWidth: 1,
+      }}
+    />
+    <path
+      d="M0 0h288v160H0z"
+      style={{
+        fill: "#d0e4ef",
+      }}
+    />
+    <path
+      d="M288 0v155c-153.066-1.133-150.563-21.667-198.54-27-29.503-3.28-89.323 0-89.323 0L0 0z"
+      style={{
+        fill: "#e8f0e4",
+      }}
+    />
+    <path
+      d="M122.4 53.35v11.3a5.35 5.35 0 0 1-5.35 5.35h-10.7a5.35 5.35 0 0 1-5.35-5.35v-11.3a5.35 5.35 0 0 1 5.35-5.35h10.7a5.35 5.35 0 0 1 5.35 5.35M148.4 58v8c0 2.208-1.792 4-4 4H131c-2.208 0-4-1.792-4-4v-8c0-2.208 1.792-4 4-4h13.4c2.208 0 4 1.792 4 4M49 84v8c0 2.208-1.792 4-4 4H31.6c-2.208 0-4-1.792-4-4v-8c0-2.208 1.792-4 4-4H45c2.208 0 4 1.792 4 4M265.101 16.5v15c0 4.139-3.361 7.5-7.5 7.5h-25.126c-4.14 0-7.5-3.361-7.5-7.5v-15c0-4.139 3.36-7.5 7.5-7.5h25.126c4.139 0 7.5 3.361 7.5 7.5M241.7 88.7h-8c-2.207 0-4-1.792-4-4V71.3c0-2.208 1.793-4 4-4h8c2.208 0 4 1.792 4 4v13.4c0 2.208-1.792 4-4 4"
+      style={{
+        fill: "#dee4e0",
+      }}
+    />
+    <path
+      d="M240 144v-34.015L176 80H80V48M176 80v30M176 80v30M80 80l-31 30"
+      style={{
+        fill: "none",
+        stroke: "#312e81",
+        strokeWidth: 5,
+      }}
+    />
+    <circle
+      cx={49}
+      cy={110}
+      r={4}
+      style={{
+        fill: "#c7d2fe",
+        stroke: "#98abeb",
+        strokeWidth: 2,
+      }}
+    />
+    <circle
+      cx={80}
+      cy={48}
+      r={4}
+      style={{
+        fill: "#c7d2fe",
+        stroke: "#98abeb",
+        strokeWidth: 2,
+      }}
+    />
+    <circle
+      cx={80}
+      cy={80}
+      r={4}
+      style={{
+        fill: "#c7d2fe",
+        stroke: "#98abeb",
+        strokeWidth: 2,
+      }}
+    />
+    <circle
+      cx={176}
+      cy={80}
+      r={4}
+      style={{
+        fill: "#c7d2fe",
+        stroke: "#98abeb",
+        strokeWidth: 2,
+      }}
+    />
+    <circle
+      cx={176}
+      cy={110}
+      r={4}
+      style={{
+        fill: "#c7d2fe",
+        stroke: "#98abeb",
+        strokeWidth: 2,
+      }}
+    />
+    <circle
+      cx={240}
+      cy={144}
+      r={4}
+      style={{
+        fill: "#c7d2fe",
+        stroke: "#98abeb",
+        strokeWidth: 2,
+      }}
+    />
+    <circle
+      cx={240}
+      cy={110}
+      r={4}
+      style={{
+        fill: "#c7d2fe",
+        stroke: "#98abeb",
+        strokeWidth: 2,
+      }}
+    />
+  </svg>
+);
+export default NetworkProjectedIllustration;
