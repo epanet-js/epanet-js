8fec3363eff9e738d67490bd2ad7b4e514a113bb
Sam Gathercole <sapagat@fastmail.com>
2026-03-10 11:53:23 +0100

    Avoid opening customer point import after login

--- src/commands/import-customer-points.tsx
+++ src/commands/import-customer-points.tsx
@@ -35,7 +35,7 @@ export const useImportCustomerPoints = () => {
             type: "importCustomerPointsWizard",
           });
         }
-      }, "importCustomerPointsWizard");
+      });
     },
     [
       setDialogState,
