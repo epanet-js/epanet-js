867bceaa4fdeeba4a0fe5cef0dd9bb895f587b1e
iteratingDesign <designteam@iterating.ca>
2026-03-11 16:20:20 +0100

    Dialogs: alert inp, alert network req, under FF

--- src/components/dialogs/alert-inp-output.tsx
+++ src/components/dialogs/alert-inp-output.tsx
@@ -1,8 +1,13 @@
-import { DialogHeader } from "src/components/dialog";
+import {
+  DialogHeader,
+  BaseModal,
+  SimpleDialogActionsNew,
+} from "src/components/dialog";
 import { useTranslate } from "src/hooks/use-translate";
 import { Form, Formik } from "formik";
 import { SimpleDialogActions } from "src/components/dialog";
 import { WarningIcon } from "src/icons";
+import { useFeatureFlag } from "src/hooks/use-feature-flags";
 
 export const AlertInpOutputDialog = ({
   onContinue,
@@ -11,7 +16,36 @@ export const AlertInpOutputDialog = ({
   onContinue: () => void;
   onClose: () => void;
 }) => {
+  const isModalsOn = useFeatureFlag("FLAG_MODALS");
   const translate = useTranslate();
+
+  if (isModalsOn) {
+    return (
+      <BaseModal
+        title={translate("alertInpOutput")}
+        size="sm"
+        isOpen={true}
+        onClose={onClose}
+        footer={
+          <SimpleDialogActionsNew
+            action={translate("understood")}
+            onAction={() => {
+              onClose();
+              onContinue();
+            }}
+          />
+        }
+      >
+        <div className="p-4 text-sm text-gray-700">
+          <p className="text-base font-semibold pb-4">
+            {translate("alertInpOutputSubtitle")}
+          </p>
+          <p>{translate("alertInpOutputDetail")}</p>
+        </div>
+      </BaseModal>
+    );
+  }
+
   return (
     <Formik
       onSubmit={() => {
--- src/components/dialogs/alert-network-required.tsx
+++ src/components/dialogs/alert-network-required.tsx
@@ -1,15 +1,21 @@
-import { DialogHeader } from "src/components/dialog";
+import {
+  DialogHeader,
+  BaseModal,
+  SimpleDialogActionsNew,
+} from "src/components/dialog";
 import { Form, Formik } from "formik";
 import { SimpleDialogActions } from "src/components/dialog";
 import { useShowWelcome } from "src/commands/show-welcome";
 import { useTranslate } from "src/hooks/use-translate";
 import { WarningIcon } from "src/icons";
+import { useFeatureFlag } from "src/hooks/use-feature-flags";
 
 export const AlertNetworkRequiredDialog = ({
   onClose,
 }: {
   onClose: () => void;
 }) => {
+  const isModalsOn = useFeatureFlag("FLAG_MODALS");
   const translate = useTranslate();
   const showWelcome = useShowWelcome();
 
@@ -17,6 +23,27 @@ export const AlertNetworkRequiredDialog = ({
     onClose();
     showWelcome({ source: "networkRequired" });
   };
+
+  if (isModalsOn) {
+    return (
+      <BaseModal
+        title={translate("alertNetworkRequired")}
+        isOpen={true}
+        onClose={onClose}
+        footer={
+          <SimpleDialogActionsNew
+            action={translate("chooseANetwork")}
+            onAction={handleChooseNetwork}
+            secondary={{ action: translate("dialog.cancel"), onClick: onClose }}
+          />
+        }
+      >
+        <div className="p-4 text-sm text-gray-700">
+          <p>{translate("alertNetworkRequiredDetail")}</p>
+        </div>
+      </BaseModal>
+    );
+  }
 
   return (
     <Formik onSubmit={handleChooseNetwork} initialValues={{}}>
