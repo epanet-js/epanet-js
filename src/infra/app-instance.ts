89f3946ed8476dc6d7863c2fd90cc3eb68a86aec
Sandra Saornil <sandra@iterating.ca>
2026-04-07 12:22:54 +0200

    Fix :: Replaced crypto.randomUUID by nanoid when generating the app instance id.

--- src/infra/app-instance.ts
+++ src/infra/app-instance.ts
@@ -1,9 +1,11 @@
+import { nanoid } from "src/lib/id";
+
 const APP_ID_KEY = "app-id";
 
 export const getAppId = (): string => {
   let id = sessionStorage.getItem(APP_ID_KEY);
   if (!id) {
-    id = crypto.randomUUID();
+    id = nanoid();
     sessionStorage.setItem(APP_ID_KEY, id);
   }
   return id;
