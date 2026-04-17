c169b5af90f972f088604f2837bba14ed10d9056
Sam Gathercole <sapagat@fastmail.com>
2026-04-17 19:22:04 +0200

    Add search in empty state panel

--- src/components/nothing-selected.tsx
+++ src/components/nothing-selected.tsx
@@ -2,18 +2,22 @@ import { memo } from "react";
 import { Keycap } from "./elements";
 import { localizeKeybinding } from "src/infra/i18n";
 import { useTranslate } from "src/hooks/use-translate";
+import { useFeatureFlag } from "src/hooks/use-feature-flags";
 
 import {
   MouseCursorDefaultIcon,
   PointerClickIcon,
   PolygonalSelectionIcon,
+  SearchIcon,
 } from "src/icons";
 import { drawingModeShorcuts } from "src/commands/set-drawing-mode";
 import { Mode } from "src/state/mode";
 import { selectionModeShortcut } from "src/commands/set-area-selection-mode";
+import { SEARCH_KEYBINDING } from "src/dialogs/cheatsheet";
 
 export const NothingSelected = memo(function NothingSelected() {
   const translate = useTranslate();
+  const isAssetSearchOn = useFeatureFlag("FLAG_ASSET_SEARCH");
 
   return (
     <div className="flex-grow flex flex-col items-center justify-center px-4 pb-4">
@@ -21,7 +25,11 @@ export const NothingSelected = memo(function NothingSelected() {
         <PointerClickIcon size={96} />
       </div>
       <p className="text-sm font-semibold py-4 text-gray-600">
-        {translate("nothingSelectedTitle")}
+        {translate(
+          isAssetSearchOn
+            ? "nothingSelectedTitleWithSearch"
+            : "nothingSelectedTitle",
+        )}
       </p>
       <div
         className="grid gap-x-2 gap-y-4 items-start text-sm text-gray-600 max-w-64"
@@ -41,6 +49,16 @@ export const NothingSelected = memo(function NothingSelected() {
           <Keycap size="xs">{localizeKeybinding(selectionModeShortcut)}</Keycap>
         </div>
         <div>{translate("nothingSelectedAreaSelect")}</div>
+        {isAssetSearchOn && (
+          <>
+            <div className="flex items-center gap-1">
+              <SearchIcon />
+              <span>/</span>
+              <Keycap size="xs">{localizeKeybinding(SEARCH_KEYBINDING)}</Keycap>
+            </div>
+            <div>{translate("nothingSelectedSearch")}</div>
+          </>
+        )}
       </div>
     </div>
   );
