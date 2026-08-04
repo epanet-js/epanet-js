76a593df21fb2911c64e566eaf6385ef00d700f5
Sandra Saornil <sandra@iterating.ca>
2026-08-04 17:54:46 +0200

    Pipe roughness:: Add helper hook to obtain roughness resolver
    GitOrigin-RevId: ecc0b3cc773a980991b594675a29853e0390ba9b

---
+++ apps/app/src/hooks/use-roughness-resolver.ts
@@ -0,0 +1,19 @@
+import { useAtomValue } from "jotai";
+import { useMemo } from "react";
+import { useFeatureFlag } from "src/hooks/use-feature-flags";
+import {
+  buildRoughnessResolver,
+  type RoughnessResolver,
+} from "src/hydraulic-model/pipe-materials";
+import { stagingModelDerivedAtom } from "src/state/derived-branch-state";
+
+export const useRoughnessResolver = (): RoughnessResolver => {
+  const isInferRoughnessOn = useFeatureFlag("FLAG_INFER_ROUGHNESS");
+  const { pipeMaterials } = useAtomValue(stagingModelDerivedAtom);
+
+  return useMemo(
+    () =>
+      buildRoughnessResolver(pipeMaterials, { enabled: isInferRoughnessOn }),
+    [pipeMaterials, isInferRoughnessOn],
+  );
+};
