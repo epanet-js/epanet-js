4ea6de0f0885692cd91a310155b9442b54eec96f
iteratingDesign <designteam@iterating.ca>
2026-09-10 16:31:44 +0200

    Dock: empty state design tweaks
    GitOrigin-RevId: 46d57169478fb40d7b6546ef1078ffe52bdb29ce

--- apps/app/src/panels/dock-empty-state.tsx
+++ apps/app/src/panels/dock-empty-state.tsx
@@ -5,26 +5,28 @@ export const DockEmptyState = () => {
   const translate = useTranslate();
 
   return (
-    <div className="h-full flex items-center justify-center gap-8 px-4">
-      <EmptyStateIcon size="2xl" className="shrink-0 text-subtle" />
-      <div className="flex flex-col gap-3">
-        <p className="text-size-heading-3 font-semibold text-subtle">
-          {translate("panels.empty.title")}
-        </p>
-        <ul className="flex flex-col gap-2 text-size-small text-subtle">
-          <li className="flex items-start gap-2">
-            <TableIcon size="md" className="shrink-0" />
-            <span className="max-w-56">
-              {translate("panels.empty.dataTables")}
-            </span>
-          </li>
-          <li className="flex items-start gap-2">
-            <HglProfileIcon size="md" className="shrink-0" />
-            <span className="max-w-56">
-              {translate("panels.empty.hglProfile")}
-            </span>
-          </li>
-        </ul>
+    <div className="h-full flex items-center justify-center px-4">
+      <div className="flex items-start gap-4">
+        <EmptyStateIcon size="2xl" className="shrink-0 text-subtle" />
+        <div className="flex flex-col gap-2">
+          <p className="text-size-heading-3 font-semibold text-subtle">
+            {translate("panels.empty.title")}
+          </p>
+          <ul className="flex flex-col gap-2 text-size-base text-subtle">
+            <li className="flex items-start gap-1">
+              <TableIcon size="md" className="shrink-0 mt-0.5" />
+              <span className="max-w-56">
+                {translate("panels.empty.dataTables")}
+              </span>
+            </li>
+            <li className="flex items-start gap-1">
+              <HglProfileIcon size="md" className="shrink-0 mt-0.5" />
+              <span className="max-w-56">
+                {translate("panels.empty.hglProfile")}
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
