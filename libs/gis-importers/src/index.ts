c06150af16731e97d1f17c6dad89147e2498b5e7
Sandra Saornil <sandra@iterating.ca>
2026-09-21 13:54:22 +0200

    Model build:: Use parsed file grouped content instead of mixed geometries features
    GitOrigin-RevId: 6048fd0cc2faa6980ea77c48d180e25ab0e5ae11

--- libs/gis-importers/src/index.ts
+++ libs/gis-importers/src/index.ts
@@ -1,6 +1,10 @@
 export { customerPointsImporter } from "./customer-points/importer";
 export { zonesImporter } from "./zones/importer";
-export type { SourceAttribute, SourceGeometry } from "./importer";
+export type {
+  GeometryGroup,
+  SourceAttribute,
+  SourceGeometry,
+} from "./importer";
 export { parseGisSource } from "./file-parsers/parse-gis-source";
 export { contentsOf } from "./file-parsers/contents";
 export { gisFormatOf, isGisAuxiliaryFile } from "./file-parsers/formats";
